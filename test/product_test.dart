de108cbfe3c80eb0491c70473da4593a1fe1d257
Louis <u19046007@gmail.com>
2021-05-27 21:08:19 +0200

    production: added product class test: added initial product test to start ci

---
+++ test/product_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:silicon_scraper/product.dart';
+
+void main()
+{
+group("product constructor parameter test", () {
+  test('availability is null attribute should be notspecified', (){
+    product gpu=new product("", "", 0, "", "", "", "", null);
+    expect( gpu.stockAvailability,availability.notSpecified);
+  });
+  test('product availability is available attribute should be available', (){
+    product gpu=new product("", "", 0, "", "", "", "", "avaIlable");
+    expect(gpu.stockAvailability,availability.available);
+  });
+  test('product availability is outofstock attribute should be outOfStock', (){
+    product gpu=new product("", "", 0, "", "", "", "", "ouTOfStock");
+    expect(gpu.stockAvailability,availability.outOfStock);
+  });
+  test('product availability is limitedstock attribute should be limitedStock', (){
+    product gpu=new product("", "", 0, "", "", "", "", "LiMitedstock");
+    expect(gpu.stockAvailability,availability.limitedStock);
+  });
+});
+}
