14759715f8df6da54e1a4093db122e2bbd4a2218
Louis <u19046007@gmail.com>
2021-05-29 00:06:03 +0200

    feat: added pages and product widget file

---
+++ lib/widgets/productWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:silicon_scraper/classes/product.dart';
+
+class ProductWidget extends StatelessWidget {
+  final product item;
+
+  const ProductWidget({Key key, this.item}) : super(key: key);
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+
+    );
+  }
+}
+
