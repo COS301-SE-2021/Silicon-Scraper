64525afc25e67232901b1479b0408eb86ad01bf9
MkSibeko <mpendu5@live.com>
2021-05-29 12:29:21 +0200

    docs: Added folder for scraper and the scraper js file allow with package-lock.json

---
+++ backend/scraper/scraper.js
@@ -0,0 +1,10 @@
+const cheerio = require("cheerio");
+const request = require("request");
+
+
+request("https://www.evetech.co.za/components/nvidia-ati-graphics-cards-21.aspx", (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        console.log($);
+    }
+})
