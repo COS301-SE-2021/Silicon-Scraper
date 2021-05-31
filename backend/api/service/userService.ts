a3ca27c8578613ade3607ad4f34718790bd531cf
ThatoJadezweni <thatojadezweni@gmail.com>
2021-06-01 00:11:23 +0200

    feat: added mock add to wish list functionality.

--- backend/api/service/userService.ts
+++ backend/api/service/userService.ts
@@ -1,7 +1,23 @@
+import {Product} from "../routes/product";
+
 export class UserService {
 
+    private products: Product[] = []
 
     addToWatchList(req) {
+        const product = {
+            "id": 1,
+            "brand": "",
+            "model": "",
+            "url": "",
+            "img": "",
+            "description": "",
+            "retailer": "",
+            "price": 0,
+            "availability": 1
+        };
+
+        this.products.push(product);
 
     }
 }
