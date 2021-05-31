fa1e4b1a50c1bee93feb08fe0c5be91d1438eae7
ThatoJadezweni <thatojadezweni@gmail.com>
2021-06-01 00:28:15 +0200

    refactor: changed routing of products to start with a /product prefix.

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -6,7 +6,7 @@ const app: express.Application = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(routes);
+app.use('/product', routes);
 app.use('/user', userRoutes);
 
 app.listen(port, () => {
