c6723b0089dd83c9207ff3d128e2665ad1804aee
v24Zer0 <vinbesse@gmail.com>
2021-05-31 15:16:09 +0200

    feat: initial implementation of product routes

--- backend/api/routes/product.ts
+++ backend/api/routes/product.ts
@@ -1,10 +1,27 @@
 import {Router} from 'express';
 const routes = Router();
 
-routes.get('/getProducts', (req, res) => {
+interface Product {
+    brand: String,
+    model: String
+    retailer: String,
+    price: Number,
+    description: String,
+    img: String,
+    url: String,
+    availability: Number
+}
 
+routes.get('/getProducts', (req, res) => {
+    res.json();
 });
 
-routes.get('/');
+routes.get('/getProductByID', (req, res) => {
+    res.json();
+});
+
+routes.get('/search', (req, res) => {
+    res.json();
+});
 
 export default routes;
