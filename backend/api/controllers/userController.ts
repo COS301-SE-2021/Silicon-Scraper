670a9af504ba12d676e9832defa3311e02c8b048
ThatoJadezweni <thatojadezweni@gmail.com>
2021-05-31 23:50:36 +0200

    feat: Added userController

---
+++ backend/api/controllers/userController.ts
@@ -0,0 +1,3 @@
+exports.addToWatchlist = (req, res) => {
+    res.send("Not implemented");
+}
--- backend/api/mocks/userMocks.ts
+++ backend/api/mocks/userMocks.ts
@@ -1,6 +1,7 @@
 //TO DO: Add this user to the PostGreSQL DB
 //as this will be the user account that will
 //be utilised to demo app functionality
+
 const users = [
     {
         id: 1,
