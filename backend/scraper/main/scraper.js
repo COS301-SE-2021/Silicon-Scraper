6f4ed0677e50deb05e10635f6d7c28fe87fc543a
MkSibeko <mpendu5@live.com>
2021-06-01 17:20:05 +0200

    feat: updating the scraper and modularizing some function

--- backend/scraper/main/scraper.js
+++ backend/scraper/main/scraper.js
@@ -1,4 +1,4 @@
-const request = require("request");
+
 const cheerio = require("cheerio");
 const axios = require("axios");
 
@@ -6,60 +6,98 @@ let url = require("../utilities/url.js");
 let EvetechSelector = require("../utilities/EvetechSelectors");
 let products = [];
 
-//Send a request to the Evetech url
-async function scrapeSilon(){
-    const html = await axios.get(url.getEveTecGpuUrl());
-    const $ = await cheerio.load(html.data);
+/**
+ *This is an array of urls to scrape
+ * @type {string[]} specific url to scrape
+ */
+let eveTech = [
+     url.getEveTecGpuUrl(),
+     url.getEveTecCpuUrl()
+]
 
+/**
+ *
+ * @param webToScrape url to
+ * @returns {array} An array of products
+ */
+const scrapeSilon = async (webToScrape) =>{
+    const html = await axios.get(webToScrape);
+    const $ = await cheerio.load(html.data);
 
     let b = 0;
 
-
     $(EvetechSelector.getTableSelector()).find(EvetechSelector.getRowSelector()).children().each((i, row) => {
-
         $(row).each((k, col)=>{
 
-            products.push({
-                image: concatUrl($(col).find(EvetechSelector.getImageSelector(b)).attr('src')),
-                title: $(col).find(EvetechSelector.getTitleSelector(b)).text().trim(),
-                price: trimPrice($(col).find(EvetechSelector.getPriceSelector()).text().trim()),
-                availability: $(col).find(EvetechSelector.getAvailabilitySelector(b)).text().trim(),
-                link: concatUrl($(col).find(EvetechSelector.getLinkSelector(b)).attr('href')),
-                retailer:"Evetech"
-            })
-            
-            b = b + 1;
-
+            addToProducts(col, b++, $);
 
         })
-
-
     })
 
-   console.log(products);
+   return products;
 
 }
+/**
+ *
+ *
+ * @param data
+ * @param index
+ * @param $
+ */
+const addToProducts = (data, index, $) =>{
+    products.push({
+        image: concatUrl($(data).find(EvetechSelector.getImageSelector(index)).attr('src')),
+        title: $(data).find(EvetechSelector.getTitleSelector(index)).text().trim(),
+        price: trimPrice($(data).find(EvetechSelector.getPriceSelector()).text().trim()),
+        availability: $(data).find(EvetechSelector.getAvailabilitySelector(index)).text().trim(),
+        link: concatUrl($(data).find(EvetechSelector.getLinkSelector(index)).attr('href')),
+        retailer:"Evetech"
+    })
+}
 
-//This function concatinates the base url to the given url
-function concatUrl(urlRES){
+/**
+ * This function concatenates the given url to the base url of the specific Website
+ * @param urlRES
+ * @returns {string}
+ */
+const concatUrl = (urlRES) =>{
+
     let base = urlRES.split('../')[1]
-    base = url.getEveTecUrl()+base;
+    base = url.getEveTecUrl() + base;
     return base;
+
 }
 
-
-//this function trims the price string
-function trimPrice(price){
-    let priceW = price.split('\n')[0];
-
-    return priceW
+/**
+ * This function cleans out the spaces and tabs from the innerHtml and returns the price ad is
+ * @param price
+ * @returns {string}
+ */
+ const trimPrice = (price) =>{
+     return price.split('\n')[0]
 }
 
+/**
+ * This function loops through the url array and calls the scrape function
+ * @returns {array} Of product objects
+ */
+module.exports.scrape = async () => {
+    for(let i = 0; i < eveTech.length; i++){
+        await scrapeSilon(eveTech[i]);
+    }
+    return products;
+}
+
+// scrape().then((data) => {
+//     console.log(data)
+// } )
 
 
-scrapeSilon().then(() => {
-    console.log("running scraper");
-})
+
+
+
+
+
 
 
 
