46c978fdac4a0d0f1738fd9fe239a572c9358a48
ThatoJadezweni <thatojadezweni@gmail.com>
2021-06-02 19:02:38 +0200

    feat: added new endpoints into userRouter file

--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router =  express.Router();
 const userController = require('../controllers/userController.js');
 
+router.post('/', userController.register_user);
+router.post('/login', userController.login_user);
+router.delete('/', userController.delete_user);
 router.post('/addToWatchlist', userController.addToWatchlist);
 
 module.exports = router;
