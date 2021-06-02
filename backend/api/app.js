6b50b74543251c6a6ac8435500fc80abc5bde2ee
v24Zer0 <vinbesse@gmail.com>
2021-06-02 17:01:04 +0200

    refactor: added file to start api to enable tests

--- backend/api/app.js
+++ backend/api/app.js
@@ -3,14 +3,9 @@ const productRoutes = require('./routes/product.js');
 const userRoutes = require('./routes/userRoutes.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
 
 module.exports = app;   // for testing
