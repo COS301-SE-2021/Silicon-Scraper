b714046246e257d13b257fab03d1a699e3cd91c1
MkSibeko <mpendu5@live.com>
2021-06-02 22:22:14 +0200

    feat: sending scraped data to the database (in processes of completion)

--- backend/scraper/main/scraperDbOp.js
+++ backend/scraper/main/scraperDbOp.js
@@ -1,10 +1,10 @@
-const { Client } = require('pg')
+const { Client, sql, Pool } = require('pg')
 const env = require('../../config.js')
 const scraper = require("../main/scraper.js");
 
 let product = []
 
-const client = new Client({
+const pool = new Pool({
     host: env.host,
     user: env.user,
     database: env.name,
@@ -12,51 +12,89 @@ const client = new Client({
     port: env.port
 })
 
-// client.connect()
-//
-// const testingConncection = () => {
-//     const query = `
-//     CREATE TABLE products (
-//         name varchar,
-//         model varchar,
-//         price float,
-//         retailer varchar,
-//         imageURL varchar,
-//         productURL varchar,
-//         availability varchar,
-//         detail JSONB
-//
-//     )
-//     `
-//     client.query(query,(err, res) =>{
-//         if(err){console.log(err);  return}
-//         console.log("created", res);
-//         client.end()
-//     })
-//
-// }
-//
-//  testingConncection()
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+})
 
 
 const getProducts =  async () => {
-    product = scraper.scrape();
-    product.then((data) =>{
-        console.log(data)
-    })
+    await scraper.scrape().then((product) => {
+        loopThroughProducts(product).then( (ronfulfilled, onrejected) =>{
+            if(ronfulfilled){
+                console.log(ronfulfilled)
+            }else{
+                console.log(onrejected)
+            }
+        })
+    });
 
 }
-getProducts()
+
+const loopThroughProducts = async (products) => {
+    for (const dataKey in products) {
+
+        console.log("opening connection for product number: ",dataKey)
+        pool.connect()
+
+        await addProducts(products[dataKey]).then(() =>{
+            pool.end()
+            console.log("inserted product number: ", dataKey, "___ connection closed")
+        })
+
+    }
+}
+
+
+getProducts().then(r => {
+    console.log(r);
+})
+
+
+
+const insertProducts = async (brand_,model_,price_,retailer_,imageurl_,producturl_,availability_,detail_) => {
+
+
+    let insertQuery =             `
+            INSERT INTO products(brand,model,price,retailer,imageurl,producturl,availability,detail)
+            VALUES ('${brand_}', '${model_}', ${price_}, '${retailer_}', '${imageurl_}', '${producturl_}', '${availability_}',  ' { "details" : [ { "date" : "${detail_.details[0].date}" , "price" : ${detail_.details[0].price} , "availability" : "${detail_.details[0].availability}" } ] } '  )
+        `
+
+
+        await client.query(
+            insertQuery, (err, res) => {
+                if (err) {
+                    console.log(err, "ERROR CAUGHT")
+
+                } else {
+                    console.log("Inserted correctly")
+                }
+            }
+        )
+
+}
 
 
 /**
  * This function populates the db with products scraped
  * @param products array from the scrape function
  */
-// const addProducts = (products) => {
-//     let insertQuery = `INSERT INTO products(name,model,price,availability,retailer,url,image)
-//                         VALUES(products.title, products.model, products.price, products.availability, products.retailer, products.url, products.image )`
-//     client.query(insertQuery, (err, res) =>{
-//
-//     })
-// }
+const addProducts = async (data) => {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let details = {
+        "date": date,
+        "price": data.price,
+        "availability": data.availability
+    }
+    let detailsList = [
+        details
+    ]
+
+    let detailsObj = { 'details' : detailsList}
+
+    await insertProducts(data.brand, data.model, data.price, data.retailer, data.image, data.link, data.availability, detailsObj).then((err, res) => {
+
+    })
+
+}
