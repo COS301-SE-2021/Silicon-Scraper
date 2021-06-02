1f756f62c482b7255e43a4ddc3196f51978fcd9a
ThatoJadezweni <thatojadezweni@gmail.com>
2021-06-02 18:48:05 +0200

    feat: created basic login function.

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -4,12 +4,21 @@ const users = [];
 const register = (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+
     const user = {
         id: users.length + 1,
         username,
         password
     };
     users.push(user);
+    res.status(201);
+};
+
+const login = (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    res.status(200).json({jwt: "jwt"});
 }
 
 const addToWatchlist = (req, res) => {
