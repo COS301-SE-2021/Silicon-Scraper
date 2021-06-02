1f0135fdc5b0e017cb93f8c61139e36b9af3ef9b
MkSibeko <mpendu5@live.com>
2021-06-02 22:18:27 +0200

    feat: Testing getting the actual img and link url function

--- test/scraper/scraperTests.js
+++ test/scraper/scraperTests.js
@@ -4,6 +4,14 @@ const fs = require("fs")
 
 
 
+//Mock url
+let url = "../repository/components/asus-rog-strix-rtx-3090-oc-24gb-gaming-300px-v1_sml.jpg";
+let exp = "https://www.evetech.co.za/repository/components/asus-rog-strix-rtx-3090-oc-24gb-gaming-300px-v1_sml.jpg"
+
+test("parsing the image or link url to link to the base url of the given website evetech" , () => {
+    expect(scraper.concatUrl(url)).toBe(exp)
+})
+
 
 test("Takes unfilterd price and trims it to fit to a double criteria" , () => {
     expect(scraper.trimPrice("R44,499/n/t/t/t/t/t/t/")).toBe(44499)
