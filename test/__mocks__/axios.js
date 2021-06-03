e225d8baabbf42313e7703cfc92614a88923757e
lesedi motoma <motomathato@gmail.com>
2021-06-03 09:09:14 +0200

    style: removed commeneted out code

--- test/__mocks__/axios.js
+++ test/__mocks__/axios.js
@@ -5,9 +5,3 @@ module.exports = {
     })
 
 };
-
-// if (url === 'https://www.evetech.co.za/components/nvidia-ati-graphics-cards-21.aspx') {
-//             return Promise.resolve({
-//                 data: 'data'
-//             });
-//         }
