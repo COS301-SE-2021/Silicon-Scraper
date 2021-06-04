24ae4dcc2ed57ca3ded950be0af485fd63048095
Louis <u19046007@gmail.com>
2021-06-04 09:08:31 +0200

    fix: changes from dev

--- lib/pages/searchPage.dart
+++ lib/pages/searchPage.dart
@@ -1,16 +1,281 @@
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:silicon_scraper/classes/product.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:silicon_scraper/widgets/productWidget.dart';
+import 'package:silicon_scraper/services/getProducts.dart';
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Search',
+      home: Search(),
+    );
+  }
+}
+
+class Search extends StatefulWidget {
+  @override
+  SearchingPage createState() => SearchingPage();
+}
+
+class SearchingPage extends State<Search> {
+  static const historyLength = 5;
+
+  List<String> _searchHistory = [
+    'history 1',
+    'history 2',
+    'history 3',
+    'history 4',
+  ];
+
+  List<String> filteredSearchHistory; //filter the search history according to what is typed
+
+  String selectedTerm;
+
+  List<String> filterSearchTerms({
+    @required String filter,
+  }) {
+    if (filter != null && filter.isNotEmpty) {
+      return _searchHistory.reversed
+          .where((term) => term.startsWith(filter))
+          .toList();
+    } else {
+      return _searchHistory.reversed.toList();
+    }
+  }
+
+  void addSearchTerm(String term) {
+
+    //put duplicated search term first
+    if (_searchHistory.contains(term)) {
+      putSearchTermFirst(term);
+      return;
+    }
+
+    _searchHistory.add(term);
+    if (_searchHistory.length > historyLength) {
+      //Least recently used
+      _searchHistory.removeRange(0, _searchHistory.length - historyLength);
+    }
+
+    //update filtered search history
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  void deleteSearchTerm(String term) {
+    _searchHistory.removeWhere((t) => t == term);
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  void putSearchTermFirst(String term) {
+    deleteSearchTerm(term);
+    addSearchTerm(term);
+  }
+
+  FloatingSearchBarController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FloatingSearchBarController();
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Center(child: Text("Search page",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),)),
-//        backgroundColor:Color(0xff0E3854) ,
-        backgroundColor: Colors.red[800],
+      body: FloatingSearchBar(
+        controller: controller,
+        body: FloatingSearchBarScrollNotifier(
+          child: SearchResultsListView(
+            searchTerm: selectedTerm,
+          ),
+        ),
+        transition: CircularFloatingSearchBarTransition(),
+        physics: BouncingScrollPhysics(),
+        title: Text(
+          selectedTerm ?? 'Search Products',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        hint: 'Search...',
+        actions: [
+          FloatingSearchBarAction.searchToClear(),
+        ],
+        onQueryChanged: (query) {
+          setState(() {
+            filteredSearchHistory = filterSearchTerms(filter: query);
+          });
+        },
+        onSubmitted: (query) {
+          setState(() {
+            addSearchTerm(query);
+            selectedTerm = query;
+          });
+          controller.close();
+        },
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Material(
+              color: Colors.white,
+              elevation: 4,
+              child: Builder(
+                builder: (context) {
+                  if (filteredSearchHistory.isEmpty &&
+                      controller.query.isEmpty) {
+                    return Container(
+                      height: 56,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Type to search for a product...',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    );
+                  } else if (filteredSearchHistory.isEmpty) {
+                    return ListTile(
+                      title: Text(controller.query),
+                      leading: const Icon(Icons.search),
+                      onTap: () {
+                        setState(() {
+                          addSearchTerm(controller.query);
+                          selectedTerm = controller.query;
+                        });
+                        controller.close();
+                      },
+                    );
+                  } else {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: filteredSearchHistory
+                          .map(
+                            (term) => ListTile(
+                          title: Text(
+                            term,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          leading: const Icon(Icons.history),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.clear),
+                            onPressed: () {
+                              setState(() {
+                                deleteSearchTerm(term);
+                              });
+                            },
+                          ),
+                          onTap: () {
+                            setState(() {
+                              putSearchTermFirst(term);
+                              selectedTerm = term;
+                            });
+                            controller.close();
+                          },
+                        ),
+                      )
+                          .toList(),
+                    );
+                  }
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
+
+
+class SearchResultsListView extends StatelessWidget {
+  final String searchTerm;
+
+  const SearchResultsListView({
+    Key key,
+    @required this.searchTerm,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (searchTerm == null) {
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.search,
+              size: 64,
+            ),
+          ],
+        ),
+      );
+    }
+
+    final fsb = FloatingSearchBar.of(context);
+
+    FutureBuilder(
+      future: getProducts(),
+      builder: (BuildContext context,AsyncSnapshot snapshot){
+        if(snapshot.connectionState==ConnectionState.none)
+        {
+          return Text("no data to display");
+        }
+        else if(snapshot.data!=null){
+
+          //final List<Product> items = snapshot.data;
+          List<Product> items=[];
+          while(snapshot.hasData){
+            if (snapshot.data.contains(searchTerm)){
+              items.add(snapshot.data);
+            }
+          }
+
+          return ListView(
+              children: List.generate(
+                items.length,
+                    (index) => ProductWidget(items[index],),
+                ),
+              //ProductWidget(items[]);
+          );
+        }
+        else{
+          return Text("waiting for data");
+        }
+      },
+    );
+
+  }
+}
+
+
+
+
+
+
+
+
+
+// ListView ProductListView(BuildContext context,List<Product> items)
+// {
+//   return ListView.builder(
+//       itemCount:items.length ,
+//       itemBuilder: (_,index){
+//         return
+//           ProductWidget(item:items[index]);
+//       }
+//   );
+//
+// }
--- lib/services/getProducts.dart
+++ lib/services/getProducts.dart
@@ -46,7 +46,7 @@ ListView ProductListView(BuildContext context,List<Product> items)
     itemCount:items.length ,
       itemBuilder: (_,index){
         return
-            ProductWidget(items[index]);
+            ProductWidget( items[index]);
       }
   );
 }
