9f8d75dc4ff66a3a2c9e9a1c10a98f65ef326a6b
ThatoJadezweni <thatojadezweni@gmail.com>
2021-06-05 19:19:42 +0200

    feat: added database connection within userService file

--- backend/api/app.js
+++ backend/api/app.js
@@ -5,6 +5,7 @@ const userController = require('./users/controller/userController.js');
 const app = express();
 
 app.use(express.json());
+
 app.use('/products', productRoutes);
 app.use('/users', userController);
 
