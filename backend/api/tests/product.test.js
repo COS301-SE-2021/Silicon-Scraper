fa86829d4d2fe3d94a5bfc8492339cb3f41f1a87
v24Zer0 <vinbesse@gmail.com>
2021-06-11 08:23:29 +0200

    fix: FIxed test issues

--- backend/api/tests/product.test.js
+++ backend/api/tests/product.test.js
@@ -2,6 +2,17 @@ const request = require('supertest');
 const app = require('../app');
 const mockData = require('../mocks/productMocks');
 
+describe('incorrect route test', () => {
+    it('should return a json object with 404 status and not found message', async () => {
+        const res = await request(app)
+        .get('/products/');
+        expect(res.body).toEqual({
+            status: 404,
+            error: 'Not found'
+        });
+    })
+})
+
 describe('getProducts test', () => {
     it('should retrieve all mockData', async () => {
         const res = await request(app)
@@ -14,23 +25,19 @@ describe('getProductByID test', () => {
     it('should return the mock product with the specified ID', async () => {
         const product = mockData[1];
         const res = await request(app)
-        .get('/products/getProductByID?id=1');
+        .get('/products/getProductByID/1');
         expect(res.body).toEqual(product);
     })
-})
 
-describe('getProductByID test', () => {
     it('should return empty object as mock product with id does not exist', async () => {
         const res = await request(app)
-        .get('/products/getProductByID?id=10');
+        .get('/products/getProductByID/10');
         expect(res.body).toEqual({});
     })
-})
 
-describe('getProductByID test', () => {
     it('should return empty object as id is not a number', async () => {
         const res = await request(app)
-        .get('/products/getProductByID?id=v');
+        .get('/products/getProductByID/v');
         expect(res.body).toEqual({});
     })
 })
@@ -42,9 +49,7 @@ describe('search test', () => {
         .get('/products/search?key=asus');
         expect(res.body).toEqual(product);
     })
-})
 
-describe('search test', () => {
     it('should return empty array as brand or model is not in mock products, i.e. Nvidia', async () => {
         const res = await request(app)
         .get('/products/search?key=nvidia');
