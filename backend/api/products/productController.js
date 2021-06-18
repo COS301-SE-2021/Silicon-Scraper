423f95e9ffca4188f1da54d35f051a5083405dfc
v24Zer0 <vinbesse@gmail.com>
2021-06-18 18:33:46 +0200

    feat: fetched data from db to search

--- backend/api/products/productController.js
+++ backend/api/products/productController.js
@@ -1,18 +1,78 @@
 const repo = require('./repo')
 const mockData = require('../mocks/productMocks')
 
-const getProducts = (req, res) => {
-    let products = mockData;
-    res.json(products);
+function genSQLQuery(query) {
+    Object.keys(query).forEach((x) => {
+        console.log(`Key=${x}`);
+        switch(x) {
+            case key:
+                break;
+            case page:
+                break;
+            case limit:
+                break;
+            case retailer:
+                break;
+            case availability:
+                break;
+            case order:
+                break;
+            default:
+                break;
+        }
+    });
 }
 
-const search = (req, res) => {
-    res.json({status: 200});
+const getProducts = async (req, res) => {
+    const data = await repo.query('SELECT * FROM gpus');
+    res.json(data);
 }
 
-const getProductByID = (req, res) => {
+const search = async (req, res) => {
+    const query = req.query;
+    const queryObj = {
+        key: '',
+        page: 0,
+        limit: ''
+    };
+    Object.keys(query).forEach((x) => {
+        console.log(`Key=${x}`);
+        switch(x) {
+            case 'key':
+                queryObj.key = query[x];
+                break;
+            case 'page':
+                queryObj.page = query[x];
+                break;
+            case 'limit':
+                queryObj.limit = query[x];
+                break;
+            default:
+                break;
+        }
+    });
+
+    const sql = {
+        name: 'search',
+        text: 'SELECT * FROM gpus UNION SELECT * FROM cpus'
+    }
+    
+    const data = await repo.query(sql);
+    const result = [];
+    data.rows.forEach((x) => {
+        let value = (x.brand+' '+x.model).toLowerCase();
+        if(value.includes(queryObj.key)) {
+            result.push(x);
+        }
+    });
+    console.log(result)
+    res.json(result.slice((queryObj.page-1)*queryObj.limit, (queryObj.page)*queryObj.limit));
+}
+
+const getProductByID = async (req, res) => {
     let id = req.params.id;
     let product = {};
+    const data = await repo.query('');
     mockData.forEach((x) => {
         if (x.id == parseInt(id.toString())) {
             product = x;
