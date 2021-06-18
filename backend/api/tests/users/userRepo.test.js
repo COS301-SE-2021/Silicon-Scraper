ceb2fb22d04aab685fd75debf501523e764927f8
ThatoJadezweni <thatojadezweni@gmail.com>
2021-06-18 12:48:20 +0200

    test: finished watclist test

--- backend/api/tests/users/userRepo.test.js
+++ backend/api/tests/users/userRepo.test.js
@@ -1,7 +1,6 @@
 const db = {}
 
-const UserRepo = require('../../users/repository/userRepo.js');
-const userRepo = new UserRepo(db);
+const userRepo = require('../../users/repository/userRepo.js')(db)
 
 describe('User repository test', () => {
 
@@ -9,18 +8,19 @@ describe('User repository test', () => {
     const password = "password";
 
     beforeEach(() => {
-        db.query = jest.fn((a, b) => Promise.resolve())
+        db.oneOrNone = jest.fn((a => Promise.resolve({"id": 1, "username": "THato"})))
+        db.none = jest.fn((query) => Promise.resolve(1))
     })
 
     it('inserting a user into a database', async() => {
 
         let response = await userRepo.addUser(username, password);
-        expect(db.query.mock.calls.length).toBe(1);
+        expect(db.none.mock.calls.length).toBe(1);
     })
 
     it('getting a user from a database', async() => {
 
         let response = await userRepo.getUser(username);
-        expect(db.query.mock.calls.length).toBe(1);
+        expect(db.none.mock.calls.length).toBe(0);
     })
 })
