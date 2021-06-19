61921a90b0438b6cce556dd55718a25e62c61de5
tanadendere <tanatswa.dendere@gmail.com>
2021-06-20 00:22:23 +0200

    search + filter connected to api

--- lib/services/getProducts.dart
+++ lib/services/getProducts.dart
@@ -5,29 +5,28 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:silicon_scraper/widgets/productWidget.dart';
 
-List<Product> addProducts(List json)
-{
-    List<Product> items=[];
-    if(json.length>0)
-    {
-      for(int i=0;i<json.length;i++)
-      {
-        String name=json[i]['brand'];
-        String model=json[i]['model'];
-        double price=json[i]['price'].toDouble();
-        String retailer=json[i]['retailer'];
-        String description=json[i]['description'];
-        String url=json[i]['url'];
-        String photo=json[i]['image'];
-        String sAvailability=json[i]['availability'];
-        items.add(new Product(name, model, price, retailer, description, url, photo, sAvailability));
-      }
+List<Product> addProducts(List json) {
+  List<Product> items = [];
+  if (json.length > 0) {
+    for (int i = 0; i < json.length; i++) {
+      String name = json[i]['brand'];
+      String model = json[i]['model'];
+      double price = json[i]['price'].toDouble();
+      String retailer = json[i]['retailer'];
+      String description = json[i]['description'];
+      String url = json[i]['url'];
+      String photo = json[i]['image'];
+      String sAvailability = json[i]['availability'];
+      items.add(new Product(name, model, price, retailer, description, url,
+          photo, sAvailability));
     }
 
-    return items;
+  }
+
+  return items;
 }
 
-Future<String>_loadFromAsset() async {
+Future<String> _loadFromAsset() async {
   return await rootBundle.loadString("test/mobile/mocks/json/products.json");
 }
 
@@ -37,29 +36,24 @@ Future<dynamic> parseJson() async {
   return jsonResponse;
 }
 
-Future<List<Product>> getProducts() async
-{
-  var json= await parseJson();
+Future<List<Product>> getProducts() async {
+  var json = await parseJson();
   return addProducts(json);
 }
 
-ListView ProductListView(BuildContext context,List<Product> items)
-{
+ListView ProductListView(BuildContext context, List<Product> items) {
   return ListView.builder(
-    itemCount:items.length ,
-      itemBuilder: (_,index){
-        return
-            ProductWidget( item:items[index]);
-      }
-  );
+      itemCount: items.length,
+      itemBuilder: (_, index) {
+        return ProductWidget(item: items[index]);
+      });
 }
 
 bool containsIgnoreCase(String modelOrBrand, String query) {
   return modelOrBrand.toLowerCase().contains(query.toLowerCase());
 }
 
-List<Product> getResults(List<Product> unProcessedProducts, String query){
-
+List<Product> getResults(List<Product> unProcessedProducts, String query) {
   List<Product> products = [];
   for (int i = 0; i < unProcessedProducts.length; i++) {
     if (containsIgnoreCase(unProcessedProducts.elementAt(i).brand, query) ||
@@ -70,100 +64,82 @@ List<Product> getResults(List<Product> unProcessedProducts, String query){
   return products;
 }
 
-List<Product> testResults(List<Product> unProcessedProducts, String query){
-
-  List<Product> products = [];
-  for (int i = 0; i < unProcessedProducts.length; i++) {
-    print(1);
-    print(unProcessedProducts.elementAt(i).brand);
-  }
-  return products;
-}
-
 List<String> getSuggestions(List<Product> unProcessedProducts, String query) {
   List<String> productBrandOrModel = [];
   for (int i = 0; i < unProcessedProducts.length; i++) {
     if (containsIgnoreCase(unProcessedProducts.elementAt(i).brand, query)) {
-      productBrandOrModel
-          .add(unProcessedProducts.elementAt(i).brand);
-    } else if (containsIgnoreCase(unProcessedProducts.elementAt(i).model, query)) {
-      productBrandOrModel
-          .add(unProcessedProducts.elementAt(i).model);
+      productBrandOrModel.add(unProcessedProducts.elementAt(i).brand);
+    } else if (containsIgnoreCase(
+        unProcessedProducts.elementAt(i).model, query)) {
+      productBrandOrModel.add(unProcessedProducts.elementAt(i).model);
     }
   }
   return productBrandOrModel;
 }
 
-double getMaxPrice(List<Product> products) {
-  double maxPrice = 0.0;
-  for (int i = 0; i < products.length; i++){
-    if (products.elementAt(i).price > maxPrice) {
-      maxPrice = products.elementAt(i).price;
-    }
-  }
-  return maxPrice;
-}
-
-double getMinPrice(List<Product> products) {
-  double minPrice = products.elementAt(0).price;
-  for (int i = 1; i < products.length; i++){
-    if (products.elementAt(i).price > minPrice) {
-      minPrice = products.elementAt(i).price;
-    }
-  }
-  return minPrice;
-}
-
-List<Product> applyFilters(List<Product> product, bool available, bool limitedStock, bool outOfStock, bool notSpecified, double maxPrice, double minPrice, bool retailer1, bool retailer2, bool retailer3) {
+List<Product> applyFilters(
+    List<Product> product,
+    bool available,
+    bool limitedStock,
+    bool outOfStock,
+    bool notSpecified,
+    double maxPrice,
+    double minPrice,
+    bool retailer1,
+    bool retailer2,
+    bool retailer3) {
   List<Product> filteredProducts = [];
 
   bool add = false;
-  for (int i = 0; i < product.length; i++){
+  for (int i = 0; i < product.length; i++) {
+    // product can only be added if it applies to all the filters chosen
+
     // check availability
+    // 1. check which filters are true
+    // 2. if its true check each product
     if (available) {
       if (product.elementAt(i).getAvailability().compareTo("available") == 0) {
         add = true;
       }
-    }
-    else if (limitedStock) {
-      if (product.elementAt(i).getAvailability().compareTo("limited stock") == 0) {
+    } else if (limitedStock) {
+      if (product.elementAt(i).getAvailability().compareTo("limited stock") ==
+          0) {
         add = true;
       }
-    }
-    else if (outOfStock) {
-      if (product.elementAt(i).getAvailability().compareTo("out of stock") == 0) {
+    } else if (outOfStock) {
+      if (product.elementAt(i).getAvailability().compareTo("out of stock") ==
+          0) {
         add = true;
       }
-    }
-    else if(notSpecified){
-      if (product.elementAt(i).getAvailability().compareTo("not specified") == 0) {
+    } else if (notSpecified) {
+      if (product.elementAt(i).getAvailability().compareTo("not specified") ==
+          0) {
         add = true;
       }
     }
 
-    //check within price range
-    if (product.elementAt(i).price >= minPrice && product.elementAt(i).price <= maxPrice){
-      add = true;
-    }
+      //check for products within price range
+      if (product.elementAt(i).price >= minPrice &&
+          product.elementAt(i).price <= maxPrice) {
+        add = true;
+      }
 
     // check retailer
-    if(retailer1){
-      if(product.elementAt(i).retailer.toLowerCase().contains("evetech")){
+    if (retailer1) {
+      if (product.elementAt(i).retailer.toLowerCase().contains("evetech")) {
         add = true;
       }
-    }
-    else if (retailer2) {
-      if(product.elementAt(i).retailer.toLowerCase().contains("wootware")){
+    } else if (retailer2) {
+      if (product.elementAt(i).retailer.toLowerCase().contains("wootware")) {
         add = true;
       }
-    }
-    else if (retailer3){
-      if(product.elementAt(i).retailer.toLowerCase().contains("pc link")){
+    } else if (retailer3) {
+      if (product.elementAt(i).retailer.toLowerCase().contains("pc link")) {
         add = true;
       }
     }
 
-    //add product that applies to the filters
+//add product that applies to the filters
     if (add) {
       filteredProducts.add(product.elementAt(i));
     }
