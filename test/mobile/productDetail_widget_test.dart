b1e904c7dc9a968cc7de942bc635128da86a569b
Louis <u19046007@gmail.com>
2021-06-19 21:25:03 +0200

    test: testing

--- test/mobile/productDetail_widget_test.dart
+++ test/mobile/productDetail_widget_test.dart
@@ -7,7 +7,7 @@ import 'mocks/json/productsjson.dart';
 
 void main() {
   var data = JSONData();
-  Product item=new Product(data['products'][0]['brand'], data["products"][0]['model'], data["products"][0]['price'], data["products"][0]['retailer'], data["products"][0]['description'], data["products"][0]['url'], data["products"][0]['image'], data["products"][0]['availability']);
+  Product item=new Product(data[0]['brand'], data[0]['model'], data[0]['price'], data[0]['retailer'], data[0]['description'], data[0]['url'], data[0]['image'], data[0]['availability']);
   group("test ProductDetail widget", () {
     testWidgets(
       'should properly mock Image.network and not crash',
