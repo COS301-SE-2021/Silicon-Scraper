0ccce1f7b362709f3b104df3f01dbd1f91f17121
tanadendere <tanatswa.dendere@gmail.com>
2021-06-19 04:49:02 +0200

    filter+search function complete w mock

--- lib/services/getProducts.dart
+++ lib/services/getProducts.dart
@@ -81,15 +81,80 @@ List<String> getSuggestions(List<Product> unProcessedProducts, String query) {
   return productBrandOrModel;
 }
 
-void openAvailabilityFilter(){
-
+double getMaxPrice(List<Product> products) {
+  double maxPrice = 0.0;
+  for (int i = 0; i < products.length; i++){
+    if (products.elementAt(i).price > maxPrice) {
+      maxPrice = products.elementAt(i).price;
+    }
+  }
+  return maxPrice;
 }
 
-void openRetailerFilter(){
-
+double getMinPrice(List<Product> products) {
+  double minPrice = products.elementAt(0).price;
+  for (int i = 1; i < products.length; i++){
+    if (products.elementAt(i).price > minPrice) {
+      minPrice = products.elementAt(i).price;
+    }
+  }
+  return minPrice;
 }
 
-void openPriceFilter(){
+List<Product> applyFilters(List<Product> product, bool available, bool limitedStock, bool outOfStock, bool notSpecified, double maxPrice, double minPrice, bool retailer1, bool retailer2, bool retailer3) {
+  List<Product> filteredProducts = [];
 
+  bool add = false;
+  for (int i = 0; i < product.length; i++){
+    // check availability
+    if (available) {
+      if (product.elementAt(i).getAvailability().compareTo("available") == 0) {
+        add = true;
+      }
+    }
+    else if (limitedStock) {
+      if (product.elementAt(i).getAvailability().compareTo("limited stock") == 0) {
+        add = true;
+      }
+    }
+    else if (outOfStock) {
+      if (product.elementAt(i).getAvailability().compareTo("out of stock") == 0) {
+        add = true;
+      }
+    }
+    else if(notSpecified){
+      if (product.elementAt(i).getAvailability().compareTo("not specified") == 0) {
+        add = true;
+      }
+    }
+
+    //check within price range
+    if (product.elementAt(i).price >= minPrice && product.elementAt(i).price <= maxPrice){
+      add = true;
+    }
+
+    // check retailer
+    if(retailer1){
+      if(product.elementAt(i).retailer.toLowerCase().contains("evetech")){
+        add = true;
+      }
+    }
+    else if (retailer2) {
+      if(product.elementAt(i).retailer.toLowerCase().contains("wootware")){
+        add = true;
+      }
+    }
+    else if (retailer3){
+      if(product.elementAt(i).retailer.toLowerCase().contains("pc link")){
+        add = true;
+      }
+    }
+
+    //add product that applies to the filters
+    if (add) {
+      filteredProducts.add(product.elementAt(i));
+    }
+    add = false;
+  }
+  return filteredProducts;
 }
-
