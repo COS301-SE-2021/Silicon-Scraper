2428c5fcf2744a2855eefbae500d1d7ad39f2016
Louis <u19046007@gmail.com>
2021-07-11 16:06:06 +0200

    feat: footer added to sliding panel

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -4,5 +4,13 @@ Color deepBlue= Color(0xff011627);
 Color beige= Color(0xffC8AD7F);
 Color myBlue=Color(0xff0E3854);
 Color myLightBlue=Color(0xff067EED);
-Color myOrange=Color(0xffFF7C1F);
+Color myOrange=const Color(0xffFF7C1F);
 Color powederBlue=Color(0xffC1E6FF);
+
+
+MaterialColor theOrange = const MaterialColor(
+  0xffFF7C1F,
+  const <int, Color>{
+    500: const Color(0xffFF7C1F),
+  },
+);
--- lib/widgets/productDetailWidget.dart
+++ lib/widgets/productDetailWidget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:silicon_scraper/models/product.dart';
 import 'package:silicon_scraper/services/watchListService.dart';
+import 'package:silicon_scraper/theme/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
@@ -10,21 +11,13 @@ class ProductDetailWidget extends StatefulWidget {
 
   final Product item;
   ProductDetailWidget(this.item);
-
   get photo => item.image;
-
   get retailer => item.retailer;
-
   get price => item.price;
-
   get Availabilitytext => item.getAvailabilityText(20,TextAlign.left);
-
   get brand => item.brand;
-
   get model => item.model;
-
   get desctiption => item.description;
-
   get url => item.url;
 
   @override
@@ -37,7 +30,7 @@ class _ProductDetailWidgetState extends State<ProductDetailWidget> {
   Product item;
   _ProductDetailWidgetState(this.item);
   bool inWatch;
-  double buttonHeight= 220;
+  double buttonHeight= 200;
 
   @override
   void initState() {
@@ -47,8 +40,8 @@ class _ProductDetailWidgetState extends State<ProductDetailWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final panelClose=MediaQuery.of(context).size.height/2.8;
-    final panelOpen=MediaQuery.of(context).size.height*0.7;
+    final panelClose=MediaQuery.of(context).size.height/3;
+    final panelOpen=MediaQuery.of(context).size.height*0.8;
     return Scaffold(
       appBar: AppBar(
         title:
@@ -56,6 +49,7 @@ class _ProductDetailWidgetState extends State<ProductDetailWidget> {
         centerTitle: true,
       ),
       body: Container(
+        color: Colors.grey[300],
         child: Stack(
 //          overflow: Overflow.visible,
           children: [
@@ -71,35 +65,51 @@ class _ProductDetailWidgetState extends State<ProductDetailWidget> {
         Stack(
           children: [
             SlidingUpPanel(
+              padding:EdgeInsets.fromLTRB(0, 0, 0, 50) ,
+
               onPanelSlide: (position)=>setState(()
               {
                 final panelMaxScrollExtent=panelOpen-panelClose;
-                buttonHeight=position*panelMaxScrollExtent+220;
+                buttonHeight=position*panelMaxScrollExtent+200;
               }),
-          backdropEnabled: true,
-          minHeight:panelClose,
-          maxHeight: panelOpen,
-//          padding: EdgeInsets.fromLTRB(5, 50, 5, 0),
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
-          header:Container(
-            width:MediaQuery.of(context).size.width ,
-            padding: EdgeInsets.only(top:10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("${widget.brand +widget.model}",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,),textAlign: TextAlign.center,),
-              ],
-            ),
-          ),
-          panel: Column(
-            children: [
-
-              Container(
-                padding: EdgeInsets.fromLTRB(5, 50, 5, 0),
-                child: Text("${widget.desctiption}"),
+              backdropEnabled: true,
+              minHeight:panelClose,
+              maxHeight: panelOpen,
+              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+              header:Container(
+                width:MediaQuery.of(context).size.width ,
+                padding: EdgeInsets.only(top:10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("${widget.brand +widget.model}",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,),textAlign: TextAlign.center,),
+                  ],
+                ),
               ),
-            ],
-          ),
+//              footer:Container(
+//                height: 40,
+//                width: MediaQuery.of(context).size.width,
+////                padding: EdgeInsets.fromLTRB(5, 0, 5, 80),
+//                child:Row(
+//                  children: [
+//                    Container(
+//                      width: MediaQuery.of(context).size.width/2,
+//                      child: Text('R${widget.price}'),
+//                    ),
+//                    Container(
+//                        child: ElevatedButton(
+//                          onPressed: () { },
+//                          child: Icon(Icons.web),
+//                        )
+//                    ),
+//                  ],
+//                ) ,
+//              ),
+              panel: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+                  child: Text("${widget.desctiption+widget.desctiption+widget.desctiption+widget.desctiption}")
+              ),
 
             ),
             Positioned(
@@ -109,68 +119,81 @@ class _ProductDetailWidgetState extends State<ProductDetailWidget> {
                   child: Icon(Icons.bookmark), onPressed: (){},
                 )
             ),
+          Positioned(
+            bottom: 0,
+            right:10,
+            left:10,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide( //                    <--- top side
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                ),
+              ),
+//              color: Colors.grey,
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+//                padding: EdgeInsets.fromLTRB(5, 0, 5, 80),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: (MediaQuery.of(context).size.width/2)-20,
+                    child: Text('R ${widget.price.toStringAsFixed(2)}',style: TextStyle(fontSize: 25,fontWeight: FontWeight.w400),),
+                  ),
+                  Container(
+                      child: FlatButton(
+                        onPressed: () { },
+//                       style: ButtonStyle(
+//                          backgroundColor: MaterialStateProperty.all(myOrange),
+//                        ),
+                      color: myOrange,
+                        child: Row(
+                          children:[
+                            Icon(Icons.web,color: Colors.white),
+                            SizedBox(width: 5,),
+                            Text("VISIT SITE",style: TextStyle(fontSize: 18,color: Colors.white),)
+                          ]
+                        ),
+                      )
+                  ),
+                ],
+              ) ,
+            ),
+          )
         ]
         ),
           ],
         ),
       ),
+//      persistentFooterButtons: [
+//////        ElevatedButton(onPressed: (){
+//////        setState(()async {
+//////          if(!inWatch)
+//////          {
+//////            inWatch=await watch.addRequest(item);
+//////          }
+//////          else if(inWatch)
+//////          {
+//////            inWatch=await watch.removeRequest(item);
+//////          }
+//////        });
+//////      },  child:Icon(Icons.bookmark_outline_rounded,color: Colors.white,))
+////        Container(
+////        child: Text('R${widget.price}'),
+////        ),
+////        ElevatedButton(child: Icon(Icons.web,color:Colors.orange),
+////          onPressed: ()async{
+////            var url = widget.url;
+////            return await launch(url);
+////
+////          },
+////            )
+////      ],
     );
   }
 
 }
 
-//old code
-//      body:Container(
-//        width:MediaQuery.of(context).size.width,
-//        height: MediaQuery.of(context).size.height/0.5,
-//        child: ListView(
-////          mainAxisAlignment: MainAxisAlignment.start,
-//          children: [
-//            Container(
-//              child: Text("${widget.brand}",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,),textAlign: TextAlign.center,),
-//            ),
-//            Container(
-////              color: Colors.grey,
-//              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-//              height: MediaQuery.of(context).size.height/3,
-//              width: MediaQuery.of(context).size.width,
-//              child: Image.network('${widget.photo}',),
-//            ),
-//            SizedBox(height: 20,),
-//            Container(
-//              child: Text("${widget.model}",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),textAlign: TextAlign.center,),
-//            ),
-//            SizedBox(height: 10,),
-//            Container(
-//              padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-//              child: Text("${widget.desctiption}",style: TextStyle(fontWeight: FontWeight.bold,),textAlign: TextAlign.center),
-//            ),
-//            Row(
-//              crossAxisAlignment: CrossAxisAlignment.start,
-//              children: [
-//                Container(
-//                  padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-//                  width: MediaQuery.of(context).size.width/2,
-//                  child: Column(
-//                    mainAxisAlignment: MainAxisAlignment.start,
-//                    children: [
-//                      Text("Retailer: ${widget.retailer}",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),textAlign: TextAlign.left,),
-//                      widget.Availabilitytext,
-//                    ],
-//                  ),
-//                ),
-//                Container(
-//                  padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-//                  width: MediaQuery.of(context).size.width/2,
-//                  child: Column(
-//                    children: [
-//                      Text("R${widget.price}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.green)),
-//                    ],
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ],
-//        )
-//      ),
-//      bottomSheet: ,
