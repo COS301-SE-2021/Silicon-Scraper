82d15276162826130a84ccfadb3816a69c5f96e1
Louis <u19046007@gmail.com>
2021-07-19 08:03:01 +0200

    feat: add watchlist view model(in progress)

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
 //        primarySwatch: Colors.green,
-      primaryColorLight: deepBlue,
         backgroundColor: Colors.grey[600],
         appBarTheme: AppBarTheme(
           brightness: Brightness.dark,
--- lib/view_models/watch_list_view_model.dart
+++ lib/view_models/watch_list_view_model.dart
@@ -1,8 +1,24 @@
-
+import 'package:silicon_scraper/models/product_model.dart';
+import 'package:silicon_scraper/services/watch_list_service.dart';
+import 'package:silicon_scraper/widgets/productWidget.dart';
+import 'package:flutter/material.dart';
 
 class WatchListViewModel
 {
+  WatchListSingleton watch= WatchListSingleton.getState();
+  List<Product> items=[];
 
 
+
+  ListView horizontalProductListView(BuildContext context,items)
+  {
+    return ListView.builder(
+        itemCount:items.length ,
+        itemBuilder: (_,index)
+        {
+          return ProductWidget(item:items[index]);
+        }
+    );
+  }
 
 }
