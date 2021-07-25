f3f0b20ce7adeb0811c3610fedd0effec62e33a8
tanadendere <tanatswa.dendere@gmail.com>
2021-07-25 20:42:00 +0200

    explore page and search, filter and sort revamp

--- lib/views/explore_view.dart
+++ lib/views/explore_view.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:silicon_scraper/services/explorePageService.dart';
+import 'package:silicon_scraper/models/product_model.dart';
+import 'package:silicon_scraper/services/explore_page_service.dart';
 import 'package:silicon_scraper/services/getProducts.dart';
+import 'package:silicon_scraper/view_models/explore_detail_view_model.dart';
+import 'package:silicon_scraper/widgets/explore_detail_widget.dart';
 //import 'package:silicon_scraper/pages/searchPage.dart'; //add search functionality
 
 class Explore extends StatefulWidget {
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   _ExploreState createState() => _ExploreState();
@@ -21,19 +17,12 @@ class Explore extends StatefulWidget {
 
 class _ExploreState extends State<Explore> {
   ExplorePageSingleton explore = ExplorePageSingleton.getState();
+  ExplorePageViewModelSingleton exploreDetail = ExplorePageViewModelSingleton.getState();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(
             "Explore",
             style: TextStyle(
@@ -58,15 +47,38 @@ class _ExploreState extends State<Explore> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-              /// recommended for you (maybe 'watchlist' items ?)
-                  ///
-                Container(
-                    width: MediaQuery.of(context).size.width,
-                    //height: MediaQuery.of(context).size.height / 39,
-                    margin: EdgeInsets.fromLTRB(20, 5, 0, 0),
-                    child: Text("CPUs", textAlign: TextAlign.left,style: TextStyle(
-                        fontSize: 26, color: Colors.black, fontWeight: FontWeight.bold),),
-                  ),
+              /// CPUs
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  //height: MediaQuery.of(context).size.height / 39,
+                  margin: EdgeInsets.fromLTRB(20, 5, 20, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "CPUs",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      RichText(
+                        textAlign: TextAlign.right,
+                        text: TextSpan(
+                            text: 'show all',
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) => ExploreDetailWidget("cpu")));
+                              },
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue,
+                            )),
+                      ),
+                    ],
+                  )),
               Container(
                 margin: EdgeInsets.only(left: 20, top: 10),
                 decoration: BoxDecoration(color: Colors.white),
@@ -77,7 +89,13 @@ class _ExploreState extends State<Explore> {
                     if (snapshot.connectionState == ConnectionState.none) {
                       return Center(child: CircularProgressIndicator());
                     } else if (snapshot.data != null) {
-                      return productHorizontalListView(context, snapshot.data);
+                      List<Product> products = exploreDetail.getExplorePageProducts(snapshot.data, "cpu");
+                      if (products.isNotEmpty) {
+                        return productHorizontalListView(context, products);
+                      }
+                      else {
+                        return noProducts(context, "CPUs");
+                      }
                     } else {
                       return Center(child: CircularProgressIndicator());
                     }
@@ -89,23 +107,51 @@ class _ExploreState extends State<Explore> {
               Container(
                   margin: EdgeInsets.all(20),
                   child: InkWell(
-                    onTap: tappedImage,
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => ExploreDetailWidget("all")));
+                    },
                     child: Image.asset(
                       'assets/images/explorePagePicture.jpg',
                       width: MediaQuery.of(context).size.width,
                       fit: BoxFit.cover,
                     ),
                   )),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    //height: MediaQuery.of(context).size.height / 39,
-                    margin: EdgeInsets.fromLTRB(20, 5, 0, 10),
-                    child: Text("GPUs", textAlign: TextAlign.left,style: TextStyle(
-                        fontSize: 26, color: Colors.black, fontWeight: FontWeight.bold),),
-                  ),
-              /// cpus
+
+              /// GPUs
               Container(
-                margin: EdgeInsets.only(left: 20),
+                  width: MediaQuery.of(context).size.width,
+                  //height: MediaQuery.of(context).size.height / 39,
+                  margin: EdgeInsets.fromLTRB(20, 5, 20, 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "GPUs",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      RichText(
+                        textAlign: TextAlign.right,
+                        text: TextSpan(
+                            text: 'show all',
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) => ExploreDetailWidget("gpu")));
+                              },
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue,
+                            )),
+                      ),
+                    ],
+                  )),
+              Container(
+                margin: EdgeInsets.only(left: 20, top: 10),
                 decoration: BoxDecoration(color: Colors.white),
                 height: MediaQuery.of(context).size.height / 4,
                 child: FutureBuilder(
@@ -114,14 +160,20 @@ class _ExploreState extends State<Explore> {
                     if (snapshot.connectionState == ConnectionState.none) {
                       return Center(child: CircularProgressIndicator());
                     } else if (snapshot.data != null) {
-                      return productHorizontalListView(context, snapshot.data);
+                      List<Product> products = exploreDetail.getExplorePageProducts(snapshot.data, "gpu");
+                      if (products.isNotEmpty) {
+                        return productHorizontalListView(context, products);
+                      }
+                      else {
+                        return noProducts(context, "GPUs");
+                      }
                     } else {
                       return Center(child: CircularProgressIndicator());
                     }
                   },
                 ),
               ),
-            ]) // This trailing comma makes auto-formatting nicer for build methods.
+            ])
             ));
   }
 }
