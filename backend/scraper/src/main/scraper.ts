e25ac7aaeeea015bce19c5c2ccf01390f6a3c1ac
lesedi motoma <motomathato@gmail.com>
2021-08-01 11:40:18 +0200

    refactor: made changes to title parser

--- backend/scraper/src/main/scraper.ts
+++ backend/scraper/src/main/scraper.ts
@@ -34,9 +34,9 @@ let jk = 0;
  * @param selector
  * @returns {array} An array of products
  */
-export const scrapeSilon = async (webToScrape: any, selector: Selectors, baseUrl: string, type:string) =>{
+export const scrapeSilon = async (webToScrape: any, selector: Selectors, baseUrl: string, type:string, timestamp: any) =>{
     const html = await axios.get(webToScrape);
-    return getWebData(html.data, selector, baseUrl, type)
+    return getWebData(html.data, selector, baseUrl, type, timestamp)
 }
 /**
  *
@@ -44,7 +44,7 @@ export const scrapeSilon = async (webToScrape: any, selector: Selectors, baseUrl
  * @param html
  */
 
-const getWebData = async (html: any, selector: Selectors, baseUrl: string, type:string) => {
+const getWebData = async (html: any, selector: Selectors, baseUrl: string, type:string, timestamp: any) => {
 
         const $ = await cheerio.load(html);
         let b = 0;
@@ -52,7 +52,7 @@ const getWebData = async (html: any, selector: Selectors, baseUrl: string, type:
         //Number of pages = number of times a request is going to happen at a specific site
         $(selector.getTableSelector()).find(selector.getRowSelector()).children().each((i: any, row: any) => {
             $(row).each((k: any, col: any) => {
-                addToProducts(b++, $, selector, baseUrl, type, col);
+                addToProducts(b++, $, selector, baseUrl, type, timestamp, col);
             })
         })
     
@@ -67,7 +67,7 @@ const getWebData = async (html: any, selector: Selectors, baseUrl: string, type:
  * @param $
  * @param selector
  */
-export const addToProducts = ( index: number, $: (arg0: any) => any[], selector: Selectors, baseUrl: string , type:string,  data?: any) =>{
+export const addToProducts = ( index: number, $: (arg0: any) => any[], selector: Selectors, baseUrl: string , type:string,  timestamp: any, data?: any) =>{
 
     let title = titleParser($(data).find(selector.getTitleSelector(index)).text().trim())
     let price = trimPrice($(data).find(selector.getPriceSelector()).text().trim())
@@ -75,7 +75,7 @@ export const addToProducts = ( index: number, $: (arg0: any) => any[], selector:
     if(price === undefined)
         return
 
-    if ( title.model === ""){
+    if ( title.model === "" || title.brand === ""){
         return 
     }
     let productsArray = {
@@ -84,21 +84,22 @@ export const addToProducts = ( index: number, $: (arg0: any) => any[], selector:
         model: title.model,
         price: price,
         availability: availability($(data).find(selector.getAvailabilitySelector(index)).text().trim()),
-        link: concatUrl($(data).find(selector.getLinkSelector(index)).attr('href'), baseUrl),
-        retailer: selector.retailer,
-        details: {
-            productDetails: [
-                {
-                    //change datetime when calling the cache data
+        date: timestamp,
+        // link: concatUrl($(data).find(selector.getLinkSelector(index)).attr('href'), baseUrl),
+        // retailer: selector.retailer,
+        // details: {
+        //     productDetails: [
+        //         {
+        //             //change datetime when calling the cache data
 
-                    datetime:date( today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()),
-                    price: trimPrice($(data).find(selector.getPriceSelector()).text().trim()),
-                    availability: availability($(data).find(selector.getAvailabilitySelector(index)).text().trim())
-                }
-            ]
-        },
+        //             datetime:date( today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()),
+        //             price: trimPrice($(data).find(selector.getPriceSelector()).text().trim()),
+        //             availability: availability($(data).find(selector.getAvailabilitySelector(index)).text().trim())
+        //         }
+        //     ]
+        // },
         type:type,
-        description:""
+        // description:""
     }
 
     if(type === "gpu") {
@@ -166,14 +167,29 @@ export const concatUrl = (urlRES: string | undefined, baseUrl: string) =>{
  */
 
 export const titleParser = (title: string) =>{
-    let detailedTitle = title.split(' ')
+    console.log(title)
+
+    let detailedTitle = title.replace(/,/g,"" ).split(' ')
     let model = ''
     let brand = '';
-    let graphics = ["GEFORCE", "RTX", "RX", "RADEON", "AMD", "GTX", "GT"]
-    for (let i = 0; i < detailedTitle.length - 1; i++) {
-        if(graphics.some(x => x === detailedTitle[i].toUpperCase())){
-            for (let k = i ; k< detailedTitle.length-1; k++){
-                model += " " + detailedTitle[k]
+    let graphics = ["GEFORCE", "RTX", "RX", "RADEON", "AMD", "GTX", "GT" ]
+    let cpus = ["RYZEN", "ATHLON", "PENTIUM", "CORE", "A12", "A10", "A8", "A6", "DUAL", "CELERON"]
+    let temp = ["A12", "A10", "A8", "A6", "i9", "i5", "i3", "i7"]
+    if(detailedTitle[0] == "RYZEN")
+        brand = "AMD "
+    for (let i = 0; i < detailedTitle.length; i++) {
+        if(detailedTitle[i].toUpperCase() === "AMD" && (cpus.some(x => detailedTitle[i+1].toUpperCase().includes(x)) || graphics.some(x => x === detailedTitle[i+1].toUpperCase())))
+            brand += detailedTitle[i] + " "
+        else if(graphics.some(x => x === detailedTitle[i].toUpperCase()) || cpus.some(x => detailedTitle[i].toUpperCase().includes(x))){
+            
+            for (let k = i ; k< detailedTitle.length; k++){
+                if(detailedTitle[k].includes("..."))
+                    break;
+                if(temp.some(x => x === detailedTitle[k])){
+                    model += " " + detailedTitle[k] + "-" + detailedTitle[k+1]
+                    k = k+2
+                }else
+                    model += " " + detailedTitle[k]
             }
             break;
         }else{
@@ -181,6 +197,8 @@ export const titleParser = (title: string) =>{
         }
 
     }
+    console.log(brand + " " + model)
+
     let detailedTitleObj = {
         'brand' : brand.slice(0,-1), //detailedTitle[0],
         'model' : model.substring(1)
@@ -199,7 +217,7 @@ export const scrape = async () => {
         for (const url of urls) {          
             for(const url_ of url.urls) {
 
-               await scrapeSilon(url_, selector, selector.getBaseUrl(), url.type);
+            //    await scrapeSilon(url_, selector, selector.getBaseUrl(), url.type);
 
             }
         }
