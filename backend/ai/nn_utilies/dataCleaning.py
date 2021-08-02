fdc69289b63e6eb3902b5693ec4c833ec52f84de
MkSibeko <mpendu5@live.com>
2021-08-02 18:38:37 +0200

    feat: renamed the cleandata function to setModel.

--- backend/ai/nn_utilies/dataCleaning.py
+++ backend/ai/nn_utilies/dataCleaning.py
@@ -3,7 +3,7 @@
 #
 #This function cleans the given data, by merging it with the model and getting rid of null rows
 #
-def cleanData(data, models):
+def setModel(data, models):
     data['availability'] = [1 if x == 'In Stock' else 0 for x in data['availability']]
 
     for dt in data.itertuples():
