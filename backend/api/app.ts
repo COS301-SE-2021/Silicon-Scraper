313a40e71f71028abb68af15637285009b5d9375
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-02 13:13:31 +0200

    included controller imports within app use routing

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import productRoutes from './routes/product';
+import userController from './users/controller/userController';
+import watchlistController from './watchlist/controller/watchlistController';
 
 const app = express();
 
 app.use(express.json());
-app.use('/products',);
-app.use('/users',);
-app.use('/watchlist',);
+app.use('/products', productRoutes);
+app.use('/users', userController);
+app.use('/watchlist', watchlistController);
 app.use((req, res, next) => {
     res.json({
         status: 404,
