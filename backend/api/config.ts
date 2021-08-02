e47cc236ed02a49c030c08e7cca85685f15c2fef
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-02 13:47:58 +0200

    changed watchlist repo to ts

--- backend/api/config.ts
+++ backend/api/config.ts
@@ -2,12 +2,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const config: Object = {
-    port : process.env.DB_PORT,
-    name : process.env.DB_NAME,
-    host : process.env.DB_HOST,
-    pw : process.env.DB_PW,
-    user : process.env.DB_USER
+interface dbConfig {
+    port: number,
+    name: string,
+    host: string,
+    pw: string,
+    user: string
+}
+
+const config: dbConfig = {
+    port : Number(process.env.DB_PORT!),
+    name : process.env.DB_NAME!,
+    host : process.env.DB_HOST!,
+    pw : process.env.DB_PW!,
+    user : process.env.DB_USER!
 }
 
 export default config;
