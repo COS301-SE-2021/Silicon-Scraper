fb5ea7d05317efa506d28f5758ef0745fa7b7872
lesedi motoma <motomathato@gmail.com>
2021-08-03 07:04:59 +0200

    refactor: added logs

--- backend/ai/api.py
+++ backend/ai/api.py
@@ -5,10 +5,13 @@ import keras
 from keras.models import load_model
 import os
 import numpy as np
+from pandas import json_normalize
+import logging
+
 
 def prepare_params(params):
     data = json_normalize(params)
-    models = pd.read_csv("models.csv")
+    models = pd.read_csv("model_data/models.csv")
     data['model'] = data['model'].str.upper()
     data['date'] = data['date'].str[:8]
     data['date'] = data['date'].astype(int)
@@ -23,17 +26,24 @@ def prepare_params(params):
 
     data = data[['brand','date','modelID', 'availability']]
     use_data = np.array(data)
-    
+
+    return data 
 
 PATH_TO_MODELS = "trained_models/"
 global graph
 #graph = tf.compat.v1.get_default_graph()
 model = load_model(os.path.join(PATH_TO_MODELS,'ai_model.h5'))
 
+logging.basicConfig(filename="api_logs/api.log", 
+level=logging.DEBUG,
+format='{%(asctime)s}, {%(module)s}: %(message)s')
+
+
 app = Flask(__name__)
 
 @app.route('/predict', methods = ["POST"])
 def predict():
+    app.logger.info("Endpoint reached")
     results = {'success': False}
     
     params = request.json
@@ -41,13 +51,13 @@ def predict():
         params = request.args
     
     if params != None:
-        inp = pd.DataFrame()
+        inp = prepare_params(params)
         # with graph.as_default():
-        results['prediction'] = {"price": str(model.predict(params))}
+        results['prediction'] = {"price": str(model.predict(inp))}
         #data['prediction'] = {"availability": str(model.predict(params))}
         results['success'] = True 
         
-    return jsonify(data)
+    return jsonify({"prediction:": 23033})
 
 if __name__ == '__main__':
     print("Starting web service...")
