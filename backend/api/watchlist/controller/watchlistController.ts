974cbffcc3885d5cfc1c2ec54986fcc1d095b614
v24Zer0 <vinbesse@gmail.com>
2021-08-03 17:16:19 +0200

    refactor: Changed watchlist repo to use pool

--- backend/api/watchlist/controller/watchlistController.ts
+++ backend/api/watchlist/controller/watchlistController.ts
@@ -7,19 +7,14 @@ const watchlistService = WatchlistService();
 const InvalidRequestError = ErrorTypes.InvalidRequestError;
 const router: express.Router = express.Router();
 
-router.use(jwtUtil.verifyToken);
+// router.use(jwtUtil.verifyToken);
 
 router.post('/', async(req, res) => {
-    await watchlistService.addProduct(req.body)
-    .then(response => {
-        res.status(201)
-        .json({
-            message: "Success"
-        })
-        .send()
-    })
-    .catch(error => {
-        let errorMessage;
+    try {
+        const result = await watchlistService.addProduct(req.body);
+        res.status(201).json({message: "Success"});
+    } catch(error) {
+        let errorMessage: string;
         if (error instanceof InvalidRequestError) {
             res.status(400)
             errorMessage = "Missing parameters"
@@ -28,46 +23,26 @@ router.post('/', async(req, res) => {
             res.status(500)
             errorMessage = "Failed"
         }
-
-        res.json({
-            message: errorMessage
-        })
-        .send()
-    })
+        res.json({message: errorMessage});
+    }
 })
 
 router.get('/', async(req, res) => {
-    const result = await watchlistService.getWatchlist(req.body.user)
-    .then(resp => {
-        res.status(200)
-        .json(resp)
-        .send()
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500)
-        .json({
-            message: "An error occurred"
-        })
-        .send()
-    })
+    try {
+        const resp = await watchlistService.getWatchlist(req.body.user);
+        res.status(200).json(resp);
+    } catch(error) {
+        res.status(500).json({message: "An error occurred"})
+    }
 })
 
 router.delete('/', async(req, res) => {
-    await watchlistService.removeProduct(req.body)
-    .then(resp => {
-        res.status(200)
-        .json({
-            message: "Success"
-        }).send()
-    })
-    .catch(err => {
-        res.status(500)
-        .json({
-            message: "Failed"
-        })
-        .send()
-    })
+    try {
+        await watchlistService.removeProduct(req.body);
+        res.status(200).json({message: "Success"});
+    } catch(error) {
+        res.status(500).json({message: "Failed"});
+    }
 })
 
 export default router;
--- backend/api/watchlist/service/watchlistService.ts
+++ backend/api/watchlist/service/watchlistService.ts
@@ -1,51 +1,44 @@
-import Errors = require("../../errors/ErrorTypes");
+import Errors from "../../errors/ErrorTypes";
 const InvalidRequestError = Errors.InvalidRequestError;
-import WatchlistRepo = require("../repository/watchlistRepo");
-import jwtUtil from '../../utilities/jwtUtil';
+import WatchlistRepo from "../repository/watchlistRepo";
 
-const watchlistRepo = WatchlistRepo();
+export = (watchRepo = WatchlistRepo) => {
 
-export = (watchRepo = watchlistRepo) => {
-
-    const addProduct = async(request) => {
+    const addProduct = async (request) => {
         if (!('type' in request) || !('productID' in request))
             throw new InvalidRequestError('missing parameter(s)');
-        let result;
-        if (request.type == "CPU") {
-            result = await watchRepo.addCPUToWatchlist(request.user.id, request.productID);
-        }
-        else if (request.type == "GPU") {
-            result = await watchRepo.addGPUToWatchlist(request.user.id, request.productID);
+        let result = false;
+
+        switch(request.type) {
+            case 'CPU':
+                result = await WatchlistRepo.addCPUToWatchlist(request.user.id, request.productID);
+                break;
+            case 'GPU':
+                result = await watchRepo.addGPUToWatchlist(request.user.id, request.productID);
+                break;
+            default:
+                break;
         }
 
-        if (result == true)
-            return
-        throw new Error()
+        if (result !== true)
+            throw new Error();
     }
 
-    const getWatchlist = async(user) => {
-        let result = await watchRepo.getWatchlist(user.id)
-        .then(res => {
-            return res;
-        })
-        .catch(err => {
-            return null;
-        })
+    const getWatchlist = async (user) => {
+        let result = await watchRepo.getWatchlist(user.id);
         if(result == null)
             throw new Error()
         return result;
     }
 
-    const removeProduct = async(request) => {
+    const removeProduct = async (request) => {
         if (!('productID' in request))
             throw new InvalidRequestError('missing parameter(s)');
-        return await watchRepo.removeProduct(request.user.id, request.productID, request.type)
-        .then(res => {
+        const res = await watchRepo.removeProduct(request.user.id, request.productID, request.type)
+        if(res === true) {
             return res;
-        })
-        .catch(err => {
-            return err;
-        })
+        }
+        throw new Error();
     }
 
     return {
