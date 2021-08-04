40501278b80a30a0b3dfe76d464503964b7c86d8
v24Zer0 <vinbesse@gmail.com>
2021-08-04 17:27:48 +0200

    fix: removed index file

--- backend/notification/src/app.ts
+++ backend/notification/src/app.ts
@@ -3,6 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+/**
+ * Use listener and wait for changes to database
+ * When a change is discovered, check if any users have the product in their watchlist
+ * Use broadcast to send messages to devices
+ * Devices can be open sockets/connections or app can poll server to check for changes
+ * Keep record of changes in array with string-based keys where index is product id
+ * changes = {
+ *  [key: string]: Product
+ * }
+ * Then to retrieve, changes[productID] = product: Product
+ */
+
+interface test {
+    [key: string]: number
+}
+
 app.get('/', (req, res) => {
     res.send('Hello there');
 });
