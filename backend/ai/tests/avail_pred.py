bc3bedfde6e219e03c38f2597580e40c058a73eb
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-05 22:07:31 +0200

    test: created test for clean data function

---
+++ backend/ai/tests/avail_pred.py
@@ -0,0 +1,13 @@
+import pytest
+import sys
+import pandas as pd
+sys.path.insert(1, '../models')
+from avail_prediction.NN_Avail_Pred_Model import clean_data, encode_data
+
+
+def test_clean_data():
+    df_prod = pd.read_csv('../mocks/cpuProductDataMock.csv')
+    df_prod_before = df_prod.copy()
+    clean_data(df_prod, '../mocks/cpuModelsMock.csv')
+    assert df_prod.equals(df_prod_before) == False
+
