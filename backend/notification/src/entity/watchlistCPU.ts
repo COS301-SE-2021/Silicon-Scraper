a889b95d844adcec75307f65075a62290d9fa5db
v24Zer0 <vinbesse@gmail.com>
2021-08-05 16:18:42 +0200

    refactor: Improved watchlist entity definition

--- backend/notification/src/entity/watchlistCPU.ts
+++ backend/notification/src/entity/watchlistCPU.ts
@@ -1,20 +1,13 @@
-import { Entity, PrimaryColumn, JoinColumn, ManyToOne } from "typeorm";
+import { Entity, PrimaryColumn, JoinColumn, ManyToOne, Index } from "typeorm";
 import { CPU } from "./cpu";
 import { User } from "./user";
 
 @Entity({name: 'watchlist_cpu', synchronize: false})
+@Index(['user_id', 'product_id'], {unique: true})
 export class watchlistCPU {
     @PrimaryColumn('uuid')
     user_id: string;
 
     @PrimaryColumn('uuid')
     product_id: string;
-    
-    @ManyToOne(type => User, user => user.id) 
-    @JoinColumn({name: 'user_id'})
-    public user!: User;
-
-    @ManyToOne(type => CPU, cpu => cpu.id) 
-    @JoinColumn({name: 'product_id'})
-    public cpu!: CPU;
 }
--- backend/notification/src/entity/watchlistGPU.ts
+++ backend/notification/src/entity/watchlistGPU.ts
@@ -1,20 +1,13 @@
-import { Entity, PrimaryColumn, JoinColumn, ManyToOne } from "typeorm";
+import { Entity, PrimaryColumn, JoinColumn, ManyToOne, Index } from "typeorm";
 import { GPU } from "./gpu";
 import { User } from "./user";
 
 @Entity({name: 'watchlist_gpu', synchronize: false})
+@Index(['user_id', 'product_id'], {unique: true})
 export class watchlistGPU {
     @PrimaryColumn('uuid')
     user_id: string;
 
     @PrimaryColumn('uuid')
     product_id: string;
-    
-    @ManyToOne(type => User, user => user.id) 
-    @JoinColumn({name: 'user_id'})
-    public user!: User;
-
-    @ManyToOne(type => GPU, gpu => gpu.id) 
-    @JoinColumn({name: 'product_id'})
-    public gpu!: GPU;
 }
