a7f73c340640ae51da099cb259b0057c02a36422
lesedi motoma <motomathato@gmail.com>
2021-08-05 13:45:44 +0200

    refactor: removed host parameter

--- backend/ai/ai/views.py
+++ backend/ai/ai/views.py
@@ -104,4 +104,4 @@ def predict():
 if __name__ == '__main__':
 
     print("Starting web service...")
-    app.run(host = '0.0.0.0', debug=True,  port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True,  port=int(os.environ.get('PORT', 5000)))
