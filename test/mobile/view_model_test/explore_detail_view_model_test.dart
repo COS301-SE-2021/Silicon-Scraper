2c69525f2aa6ae489c70e44bd3b30330d414ac1c
tanadendere <tanatswa.dendere@gmail.com>
2021-08-07 15:17:21 +0200

    test: test title for cpu product type

---
+++ test/mobile/view_model_test/explore_detail_view_model_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:silicon_scraper/view_models/explore_detail_view_model.dart';
+
+void main(){
+  ExplorePageViewModelSingleton explore = ExplorePageViewModelSingleton.getState();
+
+  group("testing explore page view model", (){
+    group("test explore detail get title", (){
+      test("test for cpu product type", (){
+        expect(explore.getTitle("cpu"), "CPUs");
+      });
+    });
+  });
+}
