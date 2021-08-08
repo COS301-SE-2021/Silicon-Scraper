afb1b5430db4c45c6de0b1c3d04c5790aac90f9c
v24Zer0 <vinbesse@gmail.com>
2021-08-08 22:17:40 +0200

    feat: created Broadcast class

---
+++ backend/notification/src/broadcast/broadcast.ts
@@ -0,0 +1,14 @@
+import { CPU } from "../entity/cpu";
+import { GPU } from "../entity/gpu";
+import { watchlistCPU } from "../entity/watchlistCPU";
+import { watchlistGPU } from "../entity/watchlistGPU";
+
+export default class Broadcast {
+    /**
+     * Send notification to client
+     */
+    public broadcast = (product: CPU | GPU) => {
+        // for every subscribed user, fetch watclist and check if product is in their watchlist
+        // if contained in watchlist, send notification
+    }
+}
