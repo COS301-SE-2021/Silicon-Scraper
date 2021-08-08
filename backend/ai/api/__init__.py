8303102a6f3cc73cd2cf0a2ba354d6c9b103e15b
lesedi motoma <motomathato@gmail.com>
2021-08-08 17:10:56 +0200

    refactor: added api config

--- backend/ai/api/__init__.py
+++ backend/ai/api/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
 
 import api.views
+
+def create_app(test_config = True):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev'
+    )
+    
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
