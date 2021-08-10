3fc15f54df7a467983437b557e8297e3d15ebaa5
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-11 01:40:11 +0200

    fix: added type conversion of supplied env port to number

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.listen(port, () => {
     console.log(`[server]: Listening on port ${port}`);
