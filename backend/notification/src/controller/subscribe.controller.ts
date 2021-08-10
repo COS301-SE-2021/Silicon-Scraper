8c1028a13b5719c9654b46f52d17ea6ea2cba55b
v24Zer0 <vinbesse@gmail.com>
2021-08-10 09:27:09 +0200

    test: changed test files to ts

---
+++ backend/notification/src/controller/subscribe.controller.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+/**
+ * 
+ * @param req Express Request object. Body includes device token and user ID
+ * @param res Sends response confirming successful subscription
+ */
+
+export const subscribe = (req: express.Request, res: express.Response) => {
+    
+}
