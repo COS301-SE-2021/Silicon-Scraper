a52211989cb5febe11ded279ff2c169c5a228be0
v24Zer0 <vinbesse@gmail.com>
2021-08-11 10:47:00 +0200

    feat: created deviceToken entity

---
+++ backend/notification/src/entity/deviceToken.ts
@@ -0,0 +1,12 @@
+import { Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import { User } from "./user";
+
+@Entity({name: "device_tokens", synchronize: false})
+export class deviceToken {
+    @PrimaryColumn()
+    token: string;
+
+    @OneToOne(() => User) 
+    @JoinColumn()
+    user: User
+}
