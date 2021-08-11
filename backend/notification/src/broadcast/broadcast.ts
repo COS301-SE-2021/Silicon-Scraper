beba43de5bfc579f055e6a5445f062dcde7036d9
v24Zer0 <vinbesse@gmail.com>
2021-08-11 19:35:25 +0200

    feat: broadcast and send implementation

--- backend/notification/src/broadcast/broadcast.ts
+++ backend/notification/src/broadcast/broadcast.ts
@@ -4,25 +4,60 @@ import { CPU } from "../entity/cpu";
 import { GPU } from "../entity/gpu";
 import { watchlistCPU } from "../entity/watchlistCPU";
 import { watchlistGPU } from "../entity/watchlistGPU";
+import { deviceToken } from '../entity/deviceToken';
 
 export default class Broadcast {
     /**
      * Send notification to client
      */
-    broadcast(product: CPU | GPU) {
+    async broadcast(product: CPU | GPU) {
         // for every subscribed user, fetch watchlist and check if product is in their watchlist
         // if contained in watchlist, send notification
-        const message: admin.messaging.TokenMessage = {
-            notification: {
-                title: 'Product Update',
-                body: ''
-            },
-            token: ''
+        const devices: deviceToken[] = [];
+        const tokens = getRepository(deviceToken);
+        switch(product.type.toLowerCase()) {
+            case 'gpu':
+                let gpuResult = await tokens.createQueryBuilder('deviceToken')
+                .innerJoinAndSelect(watchlistGPU, 'watchlistGPU', 'watchlistGPU.user.id = deviceToken.user.id')
+                .where('watchlistGPU.gpu.id = :productId', {productId: product.id}).getMany();
+                devices.push(...gpuResult);
+                break;
+            case 'cpu':
+                let cpuResult = await tokens.createQueryBuilder('deviceToken')
+                .innerJoinAndSelect(watchlistCPU, 'watchlistCPU', 'watchlistCPU.user.id = deviceToken.user.id')
+                .where('watchlistCPU.cpu.id = :productId', {productId: product.id}).getMany();
+                devices.push(...cpuResult);
+                break;
+            default:
+                return;
         }
-        this.send(message);
+        let messages = [];
+        devices.forEach((device) => {
+            if(messages.length === 500) {
+                // max messages allowed in batch send
+                this.send(messages);
+                messages = [];
+            }
+            const message: admin.messaging.TokenMessage = {
+                notification: {
+                    title: `${product.brand} ${product.model} update`,
+                    body: `R${product.price} ${product.availability}`
+                },
+                token: device.token
+            }
+            messages.push(message);
+        });
+        if(messages.length > 0)
+            this.send(messages);
     }
 
-    private send(message: admin.messaging.TokenMessage) {
-        admin.messaging().send(message);
+    private send(messages: admin.messaging.TokenMessage[]) {
+        admin.messaging().sendAll(messages)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
     }
 }
