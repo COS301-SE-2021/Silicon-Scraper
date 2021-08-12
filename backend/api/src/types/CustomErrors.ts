db807983ce401e9ead3e980900624f063f29d0a0
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-12 16:38:09 +0200

    feat: created more custom error classes

--- backend/api/src/types/CustomErrors.ts
+++ backend/api/src/types/CustomErrors.ts
@@ -1,8 +1,26 @@
-import { BadRequest } from "http-errors";
+import { BadRequest, NotFound, Unauthorized } from "http-errors";
 
 export class RequestError extends BadRequest {
 
     constructor() {
         super('Missing parameter(s) in request body');
+    }
+}
+
+export class UsernameNotFound extends NotFound {
+    constructor() {
+        super('Username not found');
+    }
+}
+
+export class ProductNotFound extends NotFound {
+    constructor() {
+        super('Product not found');
+    }
+}
+
+export class InvalidCredentials extends Unauthorized {
+    constructor() {
+        super('Invalid credentials provided');
     }
 }
