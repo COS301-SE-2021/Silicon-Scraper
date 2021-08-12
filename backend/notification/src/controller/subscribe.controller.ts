cf2975a30d07c37c5edd805673edf56dae6b700f
v24Zer0 <vinbesse@gmail.com>
2021-08-12 16:49:35 +0200

    test: subscribe route response tests

--- backend/notification/src/controller/subscribe.controller.ts
+++ backend/notification/src/controller/subscribe.controller.ts
@@ -6,21 +6,22 @@ import { User } from '../entity/user';
 const router = express.Router();
 
 router.post('/subscribe', async (req: express.Request, res: express.Response) => {
-    if(!('userID' in req.body) || !('token' in req.body)) {
+    if(!('userId' in req.body) || !('token' in req.body)) {
         res.json({status: 400});
     }
-    try {
-        const tokens = getRepository(deviceToken);
-        const device = new deviceToken();
-        device.token = req.body.token;
-        device.user = new User();
-        device.user.id = req.body.userID;
-        await tokens.save(device);
-    } catch(error) {
-        console.error(error);
-        res.json({status: 400});
-    } finally {
-        res.json({status: 200});
+    else {
+        try {
+            const tokens = getRepository(deviceToken);
+            const device = new deviceToken();
+            device.token = req.body.token;
+            device.user = new User();
+            device.user.id = req.body.userId;
+            await tokens.save(device);
+            res.json({status: 200});
+        } catch(error) {
+            console.error(error);
+            res.json({status: 400});
+        }
     }
 });
 
--- backend/notification/test/subscribe.test.ts
+++ backend/notification/test/subscribe.test.ts
@@ -3,8 +3,31 @@
  * Test subscribe route for notification server
  */
 
-describe('Basic Test', () => {
-    it('Should pass', () => {
-        expect(true).toBe(true)
-    })
-})
+import request from 'supertest';
+import typeorm from 'typeorm';
+import { app } from '../src/app';
+
+// mock Listener class
+jest.mock('pg');
+jest.mock('../src/listener/listener');
+jest.mock('typeorm', () => {
+    const actual = jest.requireActual('typeorm');
+    return {
+        ...actual,
+        getRepository: jest.fn().mockReturnValue({
+            save: jest.fn().mockImplementation(() => {})
+        }),
+    }
+});
+
+describe('subscribe route response', () => {
+    it('should return object with a status of 200', async () => {
+        const response = await request(app).post('/subscribe').send({userId: '1', token: '1'});
+        expect(response.body).toEqual({status: 200});
+    });
+
+    it('should return object with status of 400 because one or more fields are missing', async () => {
+        const response = await request(app).post('/subscribe').send({token: '1'});
+        expect(response.body).toEqual({status: 400});
+    });
+});
