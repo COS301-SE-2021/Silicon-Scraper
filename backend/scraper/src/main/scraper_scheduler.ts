0a74d17e50c414507d068b9a5912a2006f2a0dbf
MkSibeko <mpendu5@live.com>
2021-08-12 17:02:45 +0200

    re: refactored the scraper environment and added production and develop states in the scheduler.

--- backend/scraper/src/main/scraper_scheduler.ts
+++ backend/scraper/src/main/scraper_scheduler.ts
@@ -1,13 +1,21 @@
 import {CronJob} from 'cron';
 import {exec} from 'shelljs'
 
+let env = require('../../config')
 let cronJob: CronJob;
 let cron_time = '0 0,6,12,18 * * *'
 cron_time = '* * * * * *'
+let dev_script = 'ts-node scraperDbOp.ts'
+let prod_script = 'node scraperDbOp.js'
+let environment = env.scraper_env
 
 cronJob = new CronJob(cron_time, () => {
     console.log("______Webscraper running______")
-    exec('ts-node scraperDbOp.ts')
+    if(environment === dev_script){
+        exec('ts-node scraperDbOp.ts')
+    }else{
+        exec('node scraperDbOp.js')
+    }
 })
 
 cronJob.start()
