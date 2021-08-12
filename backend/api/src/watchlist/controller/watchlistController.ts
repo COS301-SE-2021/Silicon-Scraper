8ffbc3f670280b98dbf1001a85fbdc64242e4dfc
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-12 21:02:10 +0200

    fix: added return statement after calling express next

--- backend/api/src/watchlist/controller/watchlistController.ts
+++ backend/api/src/watchlist/controller/watchlistController.ts
@@ -31,7 +31,7 @@ export default class WatchlistController {
         this.router.post('/', async(req, res, next) => res.status(201).json(await this.addToWatchlist(<AddProductRequest>req.body).catch(err => next(err))));
         this.router.get('/', async(req, res, next) => res.status(200).json(await this.getWatchlist(<RetrieveWatchlistRequest>req.body).catch(err => next(err))));
         this.router.delete('/', async(req, res, next) => { 
-             await this.removeFromWatchlist(<RemoveProductRequest>req.body).catch(err => next(err)); res.status(204).send()});
+             await this.removeFromWatchlist(<RemoveProductRequest>req.body).catch(err => {next(err); return}); res.status(204).send()});
         return this.router;
     }
 }
