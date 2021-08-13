133a643669a60d4b1a017d224e2b67a9f29e1843
Louis <u19046007@gmail.com>
2021-08-13 21:11:32 +0200

    feat: local notifications

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -12,6 +12,8 @@ import 'injectors/watch_list_service_injector.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
@@ -21,6 +23,12 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print("Handling a background message: ${message.messageId}");
 }
 
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.max,
+);
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +38,42 @@ void main() async{
 
   print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||"+t);
 
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+//  var initializationSettingsAndroid = new AndroidInitializationSettings('app_icon.png');
+
+  var initializationSettingsAndroid = AndroidInitializationSettings('mipmap-mdpi/ic_launcher.png');
+
+  var initSetttings = InitializationSettings(android:initializationSettingsAndroid);
+  flutterLocalNotificationsPlugin.initialize(initSetttings);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification notification = message.notification;
+    AndroidNotification android = message.notification?.android;
+
+    // If `onMessage` is triggered with a notification, construct our own
+    // local notification to show to users using the created channel.
+    if (notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channel.description,
+              icon: android?.smallIcon,
+              // other properties...
+            ),
+          ));
+    }
+  });
 
   WatchListInjector.configure(DependencyType.PROD);
   ExplorePageInjector.configure(DependencyType.PROD);
@@ -64,6 +108,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
   Future<void> setupInteractedMessage() async {
     // Get any messages which caused the application to open from
     // a terminated state.
@@ -88,6 +133,8 @@ class _MyAppState extends State<MyApp> {
       );
     }
   }
+
+
 
   @override
   void initState() {
