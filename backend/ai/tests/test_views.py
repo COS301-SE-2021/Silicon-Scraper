2e7a6f649c7bbe189a2cfc3c309627ffcd377a7e
lesedi motoma <motomathato@gmail.com>
2021-08-13 15:46:25 +0200

    tests: added mocks

--- backend/ai/tests/test_views.py
+++ backend/ai/tests/test_views.py
@@ -1,6 +1,8 @@
 import pytest
 from api import create_app
+from api.views import prepare_params
 from flask import json
+
 
 @pytest.fixture
 def client():
@@ -8,13 +10,17 @@ def client():
     return app.test_client()
 
 
-def test_api_can_get_predictions(client):
-    res = client.get('/predict/price-and-availability', json={"brand": "MSI",
+def test_api_can_get_predictions(client, mocker):
+    params = {"brand": "MSI",
     "model": "GEFORCE GTX 1080 TI GAMING X OC",
     "availability": "Out of Stock",
     "date": "20180325112546",
     "type": "gpu",
-    "price": 15799})
+    "price": 15799}
+
+    mocker.patch('api.views.prepare_params', return_value = [params, params, 0, 0])
+    mocker.patch('api.views.load_model', return_value=[[0.41287601]])
+    res = client.get('/predict/price-and-availability', json=params)
 
     assert res.status_code == 200
     data = json.loads(res.get_data(as_text=True))
