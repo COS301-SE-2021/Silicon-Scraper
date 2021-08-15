127ebaabc490ef1bc8e8e625230444f9367e27bb
ThatoJadezweni <thatojadezweni@gmail.com>
2021-08-15 17:28:24 +0200

    fix: included missing semi-colon in setup script

--- backend/ai/setup.py
+++ backend/ai/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'api',
-    VERSION=1.0
+    VERSION=1.0,
     package = find_packages(),
     include_package_data = True,
     install_requires = [
