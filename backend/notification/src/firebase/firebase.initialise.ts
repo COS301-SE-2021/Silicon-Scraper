7dbbe5bda2288897ae17ecf9ee125934b324db08
v24Zer0 <vinbesse@gmail.com>
2021-08-17 22:24:06 +0200

    chore: heroku setup

--- backend/notification/src/firebase/firebase.initialise.ts
+++ backend/notification/src/firebase/firebase.initialise.ts
@@ -4,7 +4,8 @@ import admin from 'firebase-admin';
  * Initialize firebase app to use firebase services
  */
 export const initialise = () => {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_CREDS)
     admin.initializeApp({
-        credential: admin.credential.applicationDefault()
+        credential: admin.credential.cert(serviceAccount)
     });
 }
