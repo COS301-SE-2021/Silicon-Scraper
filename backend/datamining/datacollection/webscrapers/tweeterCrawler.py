f0096c2008c2894af48bcdbfe41be6edd8e14f70
MkSibeko <mpendu5@live.com>
2021-08-31 16:15:22 +0200

    feat: added scrape and retrieve tweets methods for the twiiter crawler

--- backend/datamining/datacollection/webscrapers/tweeterCrawler.py
+++ backend/datamining/datacollection/webscrapers/tweeterCrawler.py
@@ -16,4 +16,11 @@ class TwitterCrawler:
         self.api = tweepy.API(self.auth, wait_on_rate_limit=True)
         self.tweets = pd.DataFrame(columns=['Tweets'])
 
+    def scrapetwitter(self, search_term, since_date):
+        tweets = tweepy.Cursor(self.api.search, q=search_term, lang='en', since=since_date,
+                               tweet_mode='extended').items(2000)
+        tweets = [x.full_text for x in tweets]
+        self.tweets = self.tweets.append(pd.DataFrame(tweets, columns=['Tweets']), ignore_index=True)
 
+    def gettweets(self):
+        return self.tweets
