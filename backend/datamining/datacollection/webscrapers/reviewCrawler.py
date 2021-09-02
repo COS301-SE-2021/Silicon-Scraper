5e01bce978bed00315e93b68c9a7c34f169654b5
ThatoJadezweni <thatojadezweni@gmail.com>
2021-09-02 13:07:02 +0200

    chore: created review crawler notebook and py script

---
+++ backend/datamining/datacollection/webscrapers/reviewCrawler.py
@@ -0,0 +1,17 @@
+import BeautifulSoup
+
+class ReviewCrawler:
+
+    def __init__(self) -> None:
+        pass
+
+    def getHTML(self, url):
+        
+        pass
+
+    def save(self, articles):
+        pass
+
+    def scrape(self, url):
+        return self.save_articles(self.getHTML(url))
+
