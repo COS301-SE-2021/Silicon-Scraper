855007375237a097d062a91b1161d7daeed6d9b6
ThatoJadezweni <thatojadezweni@gmail.com>
2021-09-02 14:38:56 +0200

    feat: transferred code from notebook to py script

--- backend/datamining/datacollection/webscrapers/reviewCrawler.py
+++ backend/datamining/datacollection/webscrapers/reviewCrawler.py
@@ -1,4 +1,5 @@
-import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
 class ReviewCrawler:
 
@@ -6,12 +7,29 @@ class ReviewCrawler:
         pass
 
     def getHTML(self, url):
-        
-        pass
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        html = soup.find(id="list")
+        html = html.find_all(class_="clearfix")
+        return html
 
-    def save(self, articles):
-        pass
+    def save(self, html):
+        reviews = [{"title": item.find_next(class_="title").text, "review": item.find_next(class_="teaser").text} for item in html]
+        return reviews
+
+    def clean(self, review):
+        for condition in (("\r", ""), ("\n", ""), ("\t", "")):
+            review["title"] = review["title"].replace(*condition)
+            review["review"] = review["review"].replace(*condition)
+        return review
 
     def scrape(self, url):
-        return self.save_articles(self.getHTML(url))
+        reviews = self.save(self.getHTML(url))
+        for i in range(len(reviews)):
+            reviews[i] = self.clean(reviews[i])
+        return reviews
 
+URL = "https://www.techpowerup.com/review/?category=Graphics+Cards&manufacturer=&pp=25&order=date"
+rCrawler = ReviewCrawler()
+reviews = rCrawler.scrape(URL)
+print(reviews)
