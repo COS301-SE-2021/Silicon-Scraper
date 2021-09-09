84d95b343ac944638d02d79fd0f080eb50bbcb16
MkSibeko <mpendu5@live.com>
2021-09-09 20:59:06 +0200

    feat: implementing a sentiment controller

---
+++ backend/api/src/sentiment/controller/sentimentContoller.ts
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import { AddProductRequest, RemoveProductRequest, RetrieveWatchlistRequest } from "../../types/Requests";
+import { RetrieveWatchlistResponse } from "../../types/Responses";
+import jwtUtil from "../../utilities/jwtUtil";
+import WatchlistService from "../service/sentimentService";
+
+
+
+export default class sentimentContoller{
+
+
+
+
+}
