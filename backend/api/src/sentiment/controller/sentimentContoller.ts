d4aa2c8a67848719593c7458737d3b34ab7b7ad6
MkSibeko <mpendu5@live.com>
2021-09-09 21:40:31 +0200

    refactor: added a sentiments controller method to retrieve sentiments

--- backend/api/src/sentiment/controller/sentimentContoller.ts
+++ backend/api/src/sentiment/controller/sentimentContoller.ts
@@ -1,14 +1,22 @@
 import express, { Router } from "express";
-import { AddProductRequest, RemoveProductRequest, RetrieveWatchlistRequest } from "../../types/Requests";
-import { RetrieveWatchlistResponse } from "../../types/Responses";
+import { RetrieveSentimentRequest} from "../../types/Requests";
+import { RetrieveSentimentResponse } from "../../types/Responses";
 import jwtUtil from "../../utilities/jwtUtil";
-import WatchlistService from "../service/sentimentService";
+import SentimentService from "../service/sentimentService";
 
 
 
 export default class sentimentContoller{
 
+    private router: Router;
 
+    constructor(private readonly sentimentService: SentimentService){
+        this.router = Router;
+    }
+
+    async retrieveSentiments(request: RetrieveSentimentRequest): Promise<RetrieveSentimentResponse>{
+        return await this.sentimentService.retrieveSentiments(request)
+    }
 
 
 }
