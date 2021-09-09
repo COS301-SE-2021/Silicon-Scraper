c815ac64bfd80ae823e65367e96ef380efe1643b
MkSibeko <mpendu5@live.com>
2021-09-09 21:32:43 +0200

    refactor: added the sentiment response to the retrieve sentiment function

--- backend/api/src/sentiment/service/sentimentService.ts
+++ backend/api/src/sentiment/service/sentimentService.ts
@@ -1,6 +1,6 @@
 import { ReviewSentiment } from "../../entity/reviewSentiment"
 import {RetrieveSentimentRequest } from "../../types/Requests";
-import { RetrieveSentimentResponse } from "../../types/Responses";
+import {RetrieveSentimentResponse, RetrieveWatchlistResponse} from "../../types/Responses";
 import { getRepository, Repository } from "typeorm";
 import {RequestError} from "../../types/CustomErrors";
 
@@ -16,6 +16,10 @@ export default class SentimentService{
         sentiments = await this.sentimentRepository.createQueryBuilder('review_sentiment')
             .where({model:request.model, brand:request.brand}).select(['characteristics'])
             .getOne();
+
+        const response: RetrieveSentimentResponse = <RetrieveSentimentResponse>{};
+        response.sentiments = sentiments;
+        return response;
 
     }
 
