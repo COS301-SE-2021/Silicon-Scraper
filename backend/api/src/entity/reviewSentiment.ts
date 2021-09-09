7c6a6875a0bc81ca8055a5a781ee8aee36a3c6cb
MkSibeko <mpendu5@live.com>
2021-09-09 20:41:57 +0200

    refactor: added a ReviewSentiment class and Column for brand and model

--- backend/api/src/entity/reviewSentiment.ts
+++ backend/api/src/entity/reviewSentiment.ts
@@ -2,6 +2,11 @@ import { Entity, Column, PrimaryColumn } from "typeorm";
 
 @Entity({name: 'review_sentiment', synchronize: false})
 export class ReviewSentiment{
+    @Column()
+    brand!: string;
+
+    @Column()
+    model!: string;
 
 
 
