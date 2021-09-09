63eece450d497d63758d53a293eb0a8f2912b641
MkSibeko <mpendu5@live.com>
2021-09-09 21:31:02 +0200

    refactor: created a function that retrieves sentiments

--- backend/api/src/sentiment/service/sentimentService.ts
+++ backend/api/src/sentiment/service/sentimentService.ts
@@ -2,10 +2,21 @@ import { ReviewSentiment } from "../../entity/reviewSentiment"
 import {RetrieveSentimentRequest } from "../../types/Requests";
 import { RetrieveSentimentResponse } from "../../types/Responses";
 import { getRepository, Repository } from "typeorm";
+import {RequestError} from "../../types/CustomErrors";
 
 
 export default class SentimentService{
     constructor(
         private readonly sentimentRepository: Repository<ReviewSentiment>
     ){}
+
+    async retrieveSentiments(request: RetrieveSentimentRequest): Promise<RetrieveSentimentResponse>{
+        let sentiments: {}
+
+        sentiments = await this.sentimentRepository.createQueryBuilder('review_sentiment')
+            .where({model:request.model, brand:request.brand}).select(['characteristics'])
+            .getOne();
+
+    }
+
 }
