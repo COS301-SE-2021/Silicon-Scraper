450c9e81e76767fae0b00d25905bac969e489acd
Louis <u19046007@gmail.com>
2021-09-11 13:32:09 +0200

    feat: added moving average trendline

--- frontend/lib/views/widgets/line_chart_widget.dart
+++ frontend/lib/views/widgets/line_chart_widget.dart
@@ -1,6 +1,11 @@
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:flutter/material.dart';
-class LineChart extends StatelessWidget {
+class LineChart extends StatefulWidget {
+  @override
+  _LineChartState createState() => _LineChartState();
+}
+
+class _LineChartState extends State<LineChart> {
   @override
   Widget build(BuildContext context) {
     final List<SalesData> chartData = [
@@ -8,13 +13,31 @@ class LineChart extends StatelessWidget {
       SalesData(DateTime(2011), 50),
       SalesData(DateTime(2012), 34),
       SalesData(DateTime(2013), 70),
-      SalesData(DateTime(2014), 40)
+      SalesData(DateTime(2014), 40),
+      SalesData(DateTime(2015), 10),
+      SalesData(DateTime(2016), 50),
+      SalesData(DateTime(2017), 34),
+      SalesData(DateTime(2018), 70),
+      SalesData(DateTime(2019), 40),
+      SalesData(DateTime(2020), 10),
+      SalesData(DateTime(2021), 50),
+      SalesData(DateTime(2022), 34),
+      SalesData(DateTime(2023), 70),
+      SalesData(DateTime(2024), 40),
+      SalesData(DateTime(2025), 10),
+      SalesData(DateTime(2026), 50),
+      SalesData(DateTime(2027), 34),
+      SalesData(DateTime(2028), 70),
+      SalesData(DateTime(2029), 40)
     ];
 
     return
       Container(
+        key: UniqueKey(),
         height:MediaQuery.of(context).size.height/2.5 ,
           child: SfCartesianChart(
+              key: UniqueKey(),
+              enableAxisAnimation: true,
               tooltipBehavior:TooltipBehavior(enable: true),
             title: ChartTitle(text: "Price Trend"),
               margin:EdgeInsets.only(left:20,right:20,top:20),
@@ -22,15 +45,26 @@ class LineChart extends StatelessWidget {
               series: <ChartSeries>[
                 // Renders line chart
                 LineSeries<SalesData, DateTime>(
-                  animationDuration: 8000,
+                  key: ValueKey("Key"),
+                  animationDuration: 16000,
                     dataSource: chartData,
                     xValueMapper: (SalesData sales, _) => sales.year,
                     yValueMapper: (SalesData sales, _) => sales.sales,
                   enableTooltip: true,
             trendlines:<Trendline>[
               Trendline(
-                  type: TrendlineType.linear,
-                  color: Colors.green)
+//                forwardForecast:1,
+                  animationDuration: 8000,
+                  type: TrendlineType.movingAverage,
+                  color: Colors.red,
+                  name:'trendline',
+//                  enableTooltip: true,
+//                  markerSettings: MarkerSettings(
+//                  height:3,
+//                  width:3,
+//                  isVisible: true,
+//                ),
+              )
             ],
                     markerSettings: MarkerSettings(
                         height:3,
@@ -41,7 +75,12 @@ class LineChart extends StatelessWidget {
                         // Renders the image as marker
                         image: AssetImage("assets/images/gpu_icon.png"),*/
                     )
-                )
+                ),
+//                ColumnSeries<SalesData, DateTime>(
+//                    dataSource: chartData,
+//                    xValueMapper: (SalesData sales, _) => sales.year,
+//                    yValueMapper: (SalesData sales, _) => sales.sales
+//                )
               ]
           )
       );
