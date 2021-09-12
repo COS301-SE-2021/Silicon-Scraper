1ff6773c190e8794b4a726871136d83f81fb0aad
Louis <u19046007@gmail.com>
2021-09-12 21:13:25 +0200

    feat: add chart viewmodel and change mock data

--- frontend/lib/view_models/line_chart_view_model.dart
+++ frontend/lib/view_models/line_chart_view_model.dart
@@ -1,9 +1,21 @@
 
+import 'package:silicon_scraper/injectors/line_chart_service_injector.dart';
 import 'package:silicon_scraper/models/prediction_graph_data_model.dart';
 
 class LineChartViewModel
 {
+  LineChartInjector injector=LineChartInjector();
   List<PredictionData> data=[];
 
+  Future setData()async
+  {
+    var data=await injector.dependency.LineChartRequest("", "");
+    for(int i=0;i<data.lenght;i++)
+      {
+        data.push(PredictionData.fromJSON(data[i]));
+      }
+
+    return true;
+  }
 
 }
