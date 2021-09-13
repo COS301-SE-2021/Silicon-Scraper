f164c635dd9beedafede9113b86cb8161ce0601e
MkSibeko <mpendu5@live.com>
2021-09-13 20:52:48 +0200

    feat : Implemented a get review function that scrapes one baseurl

--- backend/datamining/datacollection/webscrapers/reviewCrawler2.py
+++ backend/datamining/datacollection/webscrapers/reviewCrawler2.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
+from decouple import config
 
 
 class ReviewCrawler:
 
     def __init__(self) -> None:
         pass
+
+    def getreview(self, product) -> {}:
+        URL = config["BASEURL_1"] + product
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
 
 
