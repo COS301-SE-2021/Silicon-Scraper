0627e9a10c33e680f79ac23494c1728b435df31d
MkSibeko <mpendu5@live.com>
2021-09-14 00:38:11 +0200

    feat: added a function that gets all the reviews, and packs them in an array of objects

--- backend/datamining/datacollection/webscrapers/reviewCrawler2.py
+++ backend/datamining/datacollection/webscrapers/reviewCrawler2.py
@@ -13,35 +13,21 @@ class ReviewCrawler:
         URL = config["BASEURL_1"] + product
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, "html.parser")
-
         results = soup.find("article")
-
         title = results.find_all(class_="review-title-medium")
-
         results = results.find(class_="pro-con")
-
         results = results.find_all("li")
-
         title = {"title": tit.text.strip() for tit in title}
-
         reviews = {"reviews": [result.text.strip() for result in results if result.text.strip() != '']}
-
         review = dict(title, **reviews)
-
         URL = config["BASEURL_1"] + product
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, "html.parser")
-
         results = soup.find("article")
-
         results = results.find(class_="pros-cons-block")
-
         results = results.find_all("li")
-
         results = [result.text.strip() for result in results if result.text.strip() != '']
-
         review['reviews'] = np.append(review['reviews'], results).tolist()
-
         return review
 
     def getproduct(self, url):
@@ -62,3 +48,14 @@ class ReviewCrawler:
             res = np.append(res, results).tolist()
 
         return res
+
+    def getreviews(self):
+        reviews = []
+        rev = self.title()
+        for r in rev:
+            revi = self.getreview(r)
+
+            if revi != {}:
+                reviews.append(revi)
+
+        return reviews
