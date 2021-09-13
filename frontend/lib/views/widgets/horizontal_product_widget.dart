166a3f5b84147932d4efa2b74c203aee045526a3
tanadendere <tanatswa.dendere@gmail.com>
2021-09-13 21:02:37 +0200

    refractor: update horizontal product widget

--- frontend/lib/views/widgets/horizontal_product_widget.dart
+++ frontend/lib/views/widgets/horizontal_product_widget.dart
@@ -1,80 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:silicon_scraper/view_models/product_view_model.dart';
+import 'package:silicon_scraper/view_models/watch_list_view_model.dart';
 import 'package:silicon_scraper/views/widgets/product_detail_widget.dart';
+import 'package:provider/provider.dart';
 
-class HorizontalProductWidget extends StatefulWidget {
-//  final Product item;
-  final ProductViewModel state;
+class HorizontalProductWidget extends StatelessWidget {
   const HorizontalProductWidget({Key key, this.state}) : super(key: key);
+  final ProductViewModel state;
 
   @override
-  _HorizontalProductWidgetState createState() => _HorizontalProductWidgetState();
-}
-
-class _HorizontalProductWidgetState extends State<HorizontalProductWidget> {
-  @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
+    return Consumer<WatchListViewModel>(
+        builder: (BuildContext context, WatchListViewModel w, Widget child) {
       return Container(
         child: Card(
-          elevation: 60.0,
           shape: RoundedRectangleBorder(
-            side: BorderSide(color: Colors.white70, width: 1),
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(10), bottomRight: Radius.circular(10)),
+            side: BorderSide(color: Colors.black12, width: 1),
           ),
-          margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+          //margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
           child: InkWell(
-            onTap: ()
-            {
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => ProductDetailWidget(widget.state)));
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ProductDetailWidget(state)));
             },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                /// image
                 Container(
-                  height: MediaQuery.of(context).size.height / 6,
-                  child: Image.network('${widget.state.item.image}',),
+                  height: MediaQuery.of(context).size.height / 5,
+                  width: MediaQuery.of(context).size.width * 0.33,
+                  child: Image.network(
+                    '${state.item.image}',
+                  ),
                 ),
+
+                /// rest of the product information
                 Container(
-                  width: MediaQuery.of(context).size.width /1.5,
-                  margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width * 0.66,
+                  height: MediaQuery.of(context).size.height / 5,
+                  margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("${widget.state.item.brand+" "+widget.state.item.model}", textAlign: TextAlign.center,style: TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold,),),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(left: 0, right: 20, top: 10),
-                            child: Column(
+                      /// product name (brand + model)
+                      Container(
+                        // width: MediaQuery.of(context).size.width * 0.66,
+                        // margin: EdgeInsets.only(top: 5),
+                        child: Text(
+                          "${state.item.brand + " " + state.item.model}",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      ///retailer
+                      Container(
+                        // width: MediaQuery.of(context).size.width * 0.66,
+                        // margin: EdgeInsets.only(top: 5),
+                        child: Text(
+                          "${state.item.retailer[0].toUpperCase()}${state.item.retailer.substring(1)}",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color.fromARGB(128, 0, 0, 0),
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      /// bookmark button, availability and price
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.66,
+                        // margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            /// bookmark button
+                            Container(
+                              margin: EdgeInsets.only(left: 15, top: 5),
+                              //alignment: Alignment.centerRight,
+                              height: MediaQuery.of(context).size.height / 20,
+                              width: MediaQuery.of(context).size.width / 12.7,
+                              child: FloatingActionButton(
+                                heroTag: state.item.id,
+                                backgroundColor: Colors.white,
+                                child: state.save,
+                                onPressed: () {
+                                  print('widget: ${state.item.id}');
+                                  state.changeState(context);
+                                },
+                              ),
+                            ),
+
+                            /// availability and price
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text("${widget.state.item.retailer}", style: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold,),
-                                  textAlign: TextAlign.left,),
-                                widget.state.item.getAvailabilityText(15, TextAlign.center),
+                                ///availability
+                                Container(
+                                  child: state.item.getAvailabilityTextFormat(),
+                                ),
+
+                                ///price
+                                Container(
+                                  //margin: EdgeInsets.only(top: 5),
+                                  child: Text(
+                                      'R ${state.item.price.toStringAsFixed(2)}',
+                                      overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black)),
+                                ),
                               ],
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 10),
-                            child: Column(
-                              children: [
-                                Text("R${widget.state.item.price}", style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.green)),
-                              ],
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
                     ],
                   ),
@@ -82,9 +137,8 @@ class _HorizontalProductWidgetState extends State<HorizontalProductWidget> {
               ],
             ),
           ),
-
         ),
       );
+    });
   }
 }
-
