28a42324f6441afa012456479a8eab97a2350124
Louis <u19046007@gmail.com>
2021-09-14 15:49:06 +0200

    feat: created sentiment model

---
+++ frontend/lib/models/sentiment_model.dart
@@ -0,0 +1,14 @@
+
+class Sentiment
+{
+  String type;
+  bool polarity;
+  double value;
+
+  Sentiment(this.type, this.polarity, this.value);
+
+  factory Sentiment.fromJSON(Map<String,dynamic>json)
+  {
+    return Sentiment(json["type"],json["polarity"],json["value"].toDouble());
+  }
+}
