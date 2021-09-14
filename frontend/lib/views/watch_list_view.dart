74b53bf2308e522277d8557d92358075d8ab9003
tanadendere <tanatswa.dendere@gmail.com>
2021-09-15 01:02:05 +0200

    refractor: update watch list appbar

--- frontend/lib/views/watch_list_view.dart
+++ frontend/lib/views/watch_list_view.dart
@@ -19,18 +19,18 @@ class _WatchListState extends State<WatchList>
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(35.0),
-        child: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Container(
-//              padding: EdgeInsets.only(bottom:15) ,
-              child: Text("Watch List",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),)
-          ),
-          centerTitle: true,
-        ),
-      ),
+//       appBar: PreferredSize(
+//         preferredSize: Size.fromHeight(35.0),
+//         child: AppBar(
+//           // Here we take the value from the MyHomePage object that was created by
+//           // the App.build method, and use it to set our appbar title.
+//           title: Container(
+// //              padding: EdgeInsets.only(bottom:15) ,
+//               child: Text("Watch List",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),)
+//           ),
+//           centerTitle: true,
+//         ),
+//       ),
     bottomNavigationBar: BottomAppBar(
 
     ),
