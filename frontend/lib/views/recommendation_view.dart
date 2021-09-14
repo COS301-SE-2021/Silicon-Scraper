bb30679c0ddf14c09bf02a8f67b9a6bc84d89905
tanadendere <tanatswa.dendere@gmail.com>
2021-09-15 01:44:14 +0200

    refractor: add structure to recommendation view

--- frontend/lib/views/recommendation_view.dart
+++ frontend/lib/views/recommendation_view.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:silicon_scraper/view_models/login_view_model.dart';
+import 'package:silicon_scraper/view_models/recommendation_view_model.dart';
 
 class Recommendation extends StatefulWidget {
-
   @override
   _RecommendationState createState() => _RecommendationState();
 }
 
 class _RecommendationState extends State<Recommendation> {
-  //RecommendationPageViewModelSingleton recommendation = RecommendationPageViewModelSingleton.getState();
+  RecommendationPageViewModelSingleton recommendation = RecommendationPageViewModelSingleton.getState();
   LoginViewModelSingleton login = LoginViewModelSingleton.getState();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("data"),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// little text
+            Container(
+              height: MediaQuery.of(context).size.height / 10,
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: Text(
+                "Recommendations based on items in your watchlist.",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 12),
+              ),
+            ),
+            // /// sort and filter buttons
+            // Container(
+            //
+            // ),
+            /// the products
+            Container(
+              child: SingleChildScrollView(
+                // child: ,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
