0365781c2741cfebd6f862c88cd79b1f3bd50b79
tanadendere <tanatswa.dendere@gmail.com>
2021-09-14 03:25:20 +0200

    refractor: add recommendation view page

---
+++ frontend/lib/views/recommendation_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:silicon_scraper/view_models/login_view_model.dart';
+
+class Recommendation extends StatefulWidget {
+
+  @override
+  _RecommendationState createState() => _RecommendationState();
+}
+
+class _RecommendationState extends State<Recommendation> {
+  //RecommendationPageViewModelSingleton recommendation = RecommendationPageViewModelSingleton.getState();
+  LoginViewModelSingleton login = LoginViewModelSingleton.getState();
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
