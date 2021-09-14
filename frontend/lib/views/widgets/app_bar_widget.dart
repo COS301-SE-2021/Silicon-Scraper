983ef35a8bf103bfba29e1355fc23a27b1c3feb1
tanadendere <tanatswa.dendere@gmail.com>
2021-09-14 02:46:45 +0200

    refractor: complete app bar

--- frontend/lib/views/widgets/app_bar_widget.dart
+++ frontend/lib/views/widgets/app_bar_widget.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 import '../search_view.dart';
 
-Widget appbar(BuildContext context, String title) {
+Widget appbar(BuildContext context, String page) {
+
+  String title = "";
+  String subtitle = "";
+  if(page.compareTo("explore") == 0){
+    title = "EXPLORE";
+    subtitle = "ALL PRODUCTS";
+  }
+  else if (page.compareTo("watchlist") == 0){
+    title = "WATCH LIST";
+    subtitle = "";
+  }
+  else if(page.compareTo("discover") == 0){
+    title = "RECOMMENDATIONS";
+    subtitle = "";
+  }
+
   return PreferredSize(
       preferredSize: Size.fromHeight(MediaQuery.of(context).size.height / 5),
       child: AppBar(
@@ -14,14 +30,13 @@ Widget appbar(BuildContext context, String title) {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment(0.0, 1.0),
-            // 10% of the width, so there are ten blinds.
             colors: <Color>[ Color(0xffffb359),
               Color(0xffffffff)],
             tileMode: TileMode.clamp,
-            // repeats the gradient over the canvas
           ),
         ),
-        child: Row(
+
+            child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -31,23 +46,24 @@ Widget appbar(BuildContext context, String title) {
               children: [
                 ///title
                 Container(
-                  margin: EdgeInsets.only(top: 20),
+                  margin: EdgeInsets.only(left: 10, top: 20),
                   width: MediaQuery.of(context).size.width / 2.2,
                   child: Text(
-                    "Title",
+                    title,
                     textAlign: TextAlign.left,
                     style: TextStyle(
-                        fontSize: 26,
+                        fontSize: 30,
                         color: Colors.black,
-                        fontWeight: FontWeight.bold),
+                        fontWeight: FontWeight.bold, letterSpacing: 3),
                   ),
                 ),
 
                 ///subtitle
                 Container(
+                  margin: EdgeInsets.only(left: 10),
                   width: MediaQuery.of(context).size.width / 2.2,
                   child: Text(
-                    "Subtitle",
+                    subtitle,
                     textAlign: TextAlign.left,
                     style: TextStyle(fontSize: 16, color: Colors.black),
                   ),
@@ -55,25 +71,34 @@ Widget appbar(BuildContext context, String title) {
 
                 ///clickable search container
                 Container(
-                  width: MediaQuery.of(context).size.width / 2.2,
-                  margin: EdgeInsets.fromLTRB(5, 5, 0, 0),
+                  width: MediaQuery.of(context).size.width / 1.8,
+                  height: MediaQuery.of(context).size.height/ 15,
+                  margin: EdgeInsets.only(left: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(57, 255, 255, 255),
+                  ),
                   child: InkWell(
                       onTap: () {
                         showSearch(context: context, delegate: ProductSearch());
                       },
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Icon(
-                            Icons.search,
-                            color: Color.fromARGB(90, 0, 0, 0),
-                          ),
-                          Text("Search",
+                          Text("  ",
                               textAlign: TextAlign.left,
                               style: TextStyle(
-                                fontSize: 14,
-                                color: Color.fromARGB(90, 0, 0, 0),
+                                fontSize: 16,
+                              )),
+                          Icon(
+                            Icons.search,
+                            color: Color.fromARGB(100, 0, 0, 0),
+                          ),
+                          Text(" Search...",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Color.fromARGB(100, 0, 0, 0),
                               ))
                         ],
                       )),
@@ -85,7 +110,7 @@ Widget appbar(BuildContext context, String title) {
             Container(
               child: Image.asset(
                 'assets/images/transparent_logo.png',
-                width: MediaQuery.of(context).size.width / 2,
+                width: MediaQuery.of(context).size.width / 2.5,
                 fit: BoxFit.cover,
               ),
             )
