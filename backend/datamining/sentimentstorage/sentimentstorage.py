a2d4ddbeaf450d696fad3f9a20ebd9938c6e30fe
MkSibeko <mpendu5@live.com>
2021-09-14 18:33:35 +0200

    feat : data storage for sentiments

--- backend/datamining/sentimentstorage/sentimentstorage.py
+++ backend/datamining/sentimentstorage/sentimentstorage.py
@@ -1,14 +1,27 @@
 import sys
 import sqlalchemy as db
+import psycopg2
 from decouple import config
 
 sys.path.append('../sentimentAnalysis')
 import sentiments
 
-engine = db.create_engine(config['DB_CONNECTION'])
-connection = engine.connect()
-metadata = db.MetaData()
+db_con = config('DB_CONNECTION')
 
-listofsentiments = sentiments.sentimentanalysis()
-query = db.insert('sentiments')
-Result = connection.execute(query, listofsentiments)
+
+class sentimentStorage:
+
+    def __init__(self, connectionPool) -> None:
+        self.engine = db.create_engine(connectionPool)
+        self.connection = self.engine.connect()
+        self.metadata = db.MetaData()
+
+    def insertSentiments(self, sentiments):
+        listofsentiments = sentiments.sentimentanalysis()
+        query = db.insert('sentiments')
+        Result = self.connection.execute(query, listofsentiments)
+        return Result
+
+
+ss = sentimentStorage(db_con)
+ss.insertSentiments(sentiments)
