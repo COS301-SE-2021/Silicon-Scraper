39580f352812568a53897e60394d1394c6b02dd8
lesedi motoma <motomathato@gmail.com>
2021-09-15 12:17:25 +0200

    chore: edited config

--- backend/recommendations/config.py
+++ backend/recommendations/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 def config():
+    load_dotenv()
+    
     db = {
-        host=os.getenv('DB_HOST'),
+        host = os.getenv('DB_HOST'),
         database=os.getenv('DB_NAME'),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PW')
