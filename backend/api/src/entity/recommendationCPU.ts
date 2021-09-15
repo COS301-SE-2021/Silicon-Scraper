c681db391b177ce7e232e6baf84426baeee3e6c0
v24Zer0 <vinbesse@gmail.com>
2021-09-15 09:14:34 +0200

    feat: typeorm entities for recommendation tables

---
+++ backend/api/src/entity/recommendationCPU.ts
@@ -0,0 +1,11 @@
+import { Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { CPU } from "./cpu";
+
+@Entity({ name: 'recommendation_cpu', synchronize: false })
+class RecommendationCPU {
+    @PrimaryColumn('uuid')
+    id!: string;
+
+    @OneToMany(type => CPU, cpu => cpu.id)
+    products!: CPU[] ;
+}
---
+++ backend/api/src/entity/recommendationGPU.ts
@@ -0,0 +1,11 @@
+import { Entity, JoinColumn, OneToMany, PrimaryColumn } from "typeorm";
+import { GPU } from "./gpu";
+
+@Entity({ name: 'recommendation_gpu', synchronize: false })
+class RecommendationGPU {
+    @PrimaryColumn('uuid')
+    id!: string;
+
+    @OneToMany(type => GPU, gpu => gpu.id)
+    products!: GPU[] ;
+}
