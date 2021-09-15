3c0e07a6088241307a1023999dd56f6b2cae2fcd
lesedi motoma <motomathato@gmail.com>
2021-09-15 11:34:17 +0200

    chore: added config file

---
+++ backend/recommendations/config.py
@@ -0,0 +1,9 @@
+import os
+
+def config():
+    db = {
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PW')
+    }
