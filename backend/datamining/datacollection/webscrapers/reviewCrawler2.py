51a20659633be2f44d369d60c581e95b5f42e3ce
ThatoJadezweni <thatojadezweni@gmail.com>
2021-09-16 17:20:41 +0200

    fix: added results error handling for nonexisting review for a certain product

--- backend/datamining/datacollection/webscrapers/reviewCrawler2.py
+++ backend/datamining/datacollection/webscrapers/reviewCrawler2.py
@@ -16,18 +16,20 @@ class ReviewCrawler:
         results = soup.find("article")
         title = results.find_all(class_="review-title-medium")
         results = results.find(class_="pro-con")
-        results = results.find_all("li")
-        title = {"title": tit.text.strip() for tit in title}
-        reviews = {"reviews": [result.text.strip() for result in results if result.text.strip() != '']}
-        review = dict(title, **reviews)
-        URL = config["BASEURL_2"] + product
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, "html.parser")
-        results = soup.find("article")
-        results = results.find(class_="pros-cons-block")
-        results = results.find_all("li")
-        results = [result.text.strip() for result in results if result.text.strip() != '']
-        review['reviews'] = np.append(review['reviews'], results).tolist()
+        review = {}
+        if results != None:
+            results = results.find_all("li")
+            title = {"title": tit.text.strip() for tit in title}
+            reviews = {"reviews": [result.text.strip() for result in results if result.text.strip() != '']}
+            review = dict(title, **reviews)
+            URL = config["BASEURL_2"] + product
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, "html.parser")
+            results = soup.find("article")
+            results = results.find(class_="pros-cons-block")
+            results = results.find_all("li")
+            results = [result.text.strip() for result in results if result.text.strip() != '']
+            review['reviews'] = np.append(review['reviews'], results).tolist()
         return review
 
     def getproduct(self, url):
