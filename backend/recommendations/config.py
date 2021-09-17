4c51ee7bf3e7766e3b3a78f096c564ecea881abb
lesedi motoma <motomathato@gmail.com>
2021-09-17 19:14:08 +0200

    refactor: edited config

--- backend/recommendations/config.py
+++ backend/recommendations/config.py
@@ -3,14 +3,17 @@ from configParser import ConfigParser
 
 def config(filename='database.ini', section='postgresql'):
     con = {}
-    host = ''
     parser = configParser()
     parser.read(filename)
  
     if parser.has_section(section):
-        params = parser.items(section)
-        for p in params:
-            con[p[0]] = p[1]
+        con = {
+            "host": parser.get(sectio,'DB_HOST'),
+            "port": parser.get(section,'DB_PORT'),
+            "database": parser.get(section,'DB_NAME'),
+            "user": parser.get(section,'DB_USER'),
+            "password": parser.get(section,'DB_PW')
+        }
 
 
     return con
