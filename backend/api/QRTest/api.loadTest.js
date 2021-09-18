9ed86b56b5a977715a2175835987f96413ffec04
v24Zer0 <vinbesse@gmail.com>
2021-09-18 21:24:31 +0200

    test: load and performance tests to fetch products

---
+++ backend/api/QRTest/api.loadTest.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 1000 },
+        { duration: '1m', target: 1000 },
+        { duration: '1m', target: 0 }
+    ]
+}
+
+function fetchProductsTest() {
+    var url = 'https://api-silicon-scraper.herokuapp.com/products';
+    var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiMzI0OThmY2UtNGM0My00YzcxLThlMDAtMzhlNDBhNDRiMjg0IiwiaWF0IjoxNjMxOTgyMTk3LCJleHAiOjE2NjM1MTgxOTd9.YxAiAT0QSEIs6O1W7fnR6_EndEPQrfFTsnY4auP2KuM';
+  
+    var params = {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    };
+
+    http.get(url, params);
+}
+
+export default function () {
+    fetchProductsTest();
+}
