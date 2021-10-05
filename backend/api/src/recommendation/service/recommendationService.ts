28555c3d1fdf94b68534efd9670275ed839d0052
v24Zer0 <vinbesse@gmail.com>
2021-10-05 18:29:16 +0200

    feat: updated recommendation service

--- backend/api/src/recommendation/service/recommendationService.ts
+++ backend/api/src/recommendation/service/recommendationService.ts
@@ -15,13 +15,15 @@ async function fetchRecommendations(id: string) {
     const wl_cpu = await fetchWatchlistCPU(id);
     const wl_gpu = await fetchWatchlistGPU(id);
     const recommendations: GPU[] | CPU[] = [];
+    const hashed_recommendations = {};
     for(let cpu of wl_cpu) {
-        const cpus = await fetchCPUs(cpu.products);
-        recommendations.push(...cpus);
+        hashed_recommendations[cpu.recommended.id] = cpu.recommended;
     }
     for(let gpu of wl_gpu) {
-        const gpus = await fetchGPUs(gpu.products);
-        recommendations.push(...gpus);
+        hashed_recommendations[gpu.recommended.id] = gpu.recommended;
+    }
+    for(let product in hashed_recommendations) {
+        recommendations.push(hashed_recommendations[product]);
     }
     return recommendations;
 }
@@ -29,7 +31,7 @@ async function fetchRecommendations(id: string) {
 async function fetchWatchlistCPU(id: string) {
     try {
         return await getRepository(RecommendationCPU).createQueryBuilder('recommendation')
-        .innerJoinAndSelect(watchlistCPU, 'wl_cpu', 'wl_cpu.product_id = recommendation.id')
+        .innerJoinAndSelect(watchlistCPU, 'wl_cpu', 'wl_cpu.product_id = recommendation.product_id')
         .where('wl_cpu.user_id = :user_id', {user_id: id})
         .getMany();
     } catch(error) {
@@ -40,7 +42,7 @@ async function fetchWatchlistCPU(id: string) {
 async function fetchWatchlistGPU(id: string) {
     try {
         return await getRepository(RecommendationGPU).createQueryBuilder('recommendation')
-        .innerJoinAndSelect(watchlistGPU, 'wl_gpu', 'wl_gpu.product_id = recommendation.id')
+        .innerJoinAndSelect(watchlistGPU, 'wl_gpu', 'wl_gpu.product_id = recommendation.product_id')
         .where('wl_gpu.user_id = :user_id', {user_id: id})
         .getMany();
     } catch(error) {
@@ -48,23 +50,11 @@ async function fetchWatchlistGPU(id: string) {
     }
 }
 
-async function fetchGPUs(productIds: string[]) {
-    return await getRepository(GPU).createQueryBuilder('gpus')
-    .where('gpus.id IN (:...ids)', {ids: productIds}).getMany();
-}
-
-async function fetchCPUs(productIds: string[]) {
-    return await getRepository(CPU).createQueryBuilder('cpus')
-    .where('cpus.id IN (:...ids)', {ids: productIds}).getMany();
-}
-
 const service = {
     getRecommendations,
     fetchRecommendations,
     fetchWatchlistCPU,
-    fetchWatchlistGPU,
-    fetchCPUs,
-    fetchGPUs
+    fetchWatchlistGPU
 }
 
 export default service;
