afbaf03efe54f9e3206323f4267fab830933e96d
lesedi motoma <motomathato@gmail.com>
2021-10-10 23:59:02 +0200

    refactor: changed query statements

--- backend/recommendations/app/Recommender.py
+++ backend/recommendations/app/Recommender.py
@@ -7,28 +7,31 @@ import math
 import uuid
 import psycopg2.extensions
 import psycopg2.extras
+from psycopg2 import sql
 from nltk.corpus import stopwords
 from configparser import ConfigParser
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
 stopwords = set(stopwords.words('english'))
-all_products = None
+products = None
 rec = None
 similarity = None
 
 cur = None
 con = None
 
-def get_wishlist(cur, con):
-    query = "SELECT * FROM watchlist_cpu"
-    cpus = pd.read_sql_query(query, con)
-    cpus = pd.DataFrame(cpus)
+
+def get_wishlist(cur, con, types):
+    query = ''
+    if types == 'cpu':
+        query = "SELECT * FROM watchlist_cpu"
+    else:
+        query = "SELECT * FROM watchlist_gpu"
     
-    query = "SELECT * FROM watchlist_gpu"
-    gpus = pd.read_sql_query(query, con)
+    table = pd.read_sql_query(query, con)
+    table = pd.DataFrame(table)
     
-    table = cpus.append(gpus)
     #any preprocessing needing to be done
     #table = remove_columns_from_products(table)
     table.drop_duplicates(subset='product_id', keep=False, inplace=True)
@@ -36,20 +39,24 @@ def get_wishlist(cur, con):
     return table 
 
 
-def get_all_products(cur, con):
-    query = "SELECT id, brand, retailer, type, description FROM cpus"
-    cpus = pd.read_sql_query(query, con)
-    cpus = pd.DataFrame(cpus)
-    
-    query = "SELECT id, brand, retailer, type, description FROM gpus"
-    gpus = pd.read_sql_query(query, con)
-    
-    table = cpus.append(gpus)
+def get_all_products(cur, con, types):
+    query = ''
+    if types == 'cpu':
+        query = "SELECT id, brand, retailer, type, description FROM cpus"
+    else:
+        query = "SELECT id, brand, retailer, type, description FROM gpus"
+
+    table = pd.read_sql_query(query, con)
+    table = pd.DataFrame(table)
     
     # any preprocessing needed to be done
     table['description_cl'] = table['description'].apply(clean_data)
     table.drop(['description'], axis=1, inplace=True)
     table.dropna(inplace=True)
+
+    table.rename(columns={'id': 'product_id'}, inplace=True)
+    table['all_features'] = table['description_cl']
+    table['all_features'] = table['all_features'].apply(lambda x: x.lower())
 
     return table
 
@@ -137,19 +144,27 @@ def get_all_product_recommendations(wishlist_description):
 
 
 
-def update_gpu(gpu_recs):
+def update_rec_table(recs, types):
     new_products = []
-   
+    if types == "cpu":
+        table_name = "recommendation_cpu"
+    else:
+        table_name = "recommendation_gpu"
+
     try:
 
-        for i, row in gpu_recs.iterrows():
-            query = ("""SELECT id FROM recommendation_gpu WHERE id = %s""")
+        for i, row in recs.iterrows():
+            query = sql.SQL("SELECT product_id FROM {} WHERE product_id = %s")
+                    .format(sql.Identifier(table_name))
+                     
+            # query = ("""SELECT product_id FROM recommendation_gpu WHERE product_id = %s""")
             cur.execute(query, (i,))
             product = cur.fetchone()
             
             if product is not None:
                  for recs in row['recommendations']:
-                    query = (""" UPDATE recommendation_gpu SET recommended_id = %s WHERE product_id = %s""")
+                    query = sql.SQL(" UPDATE {} SET recommended_id = %s WHERE product_id = %s")
+                                .format(sql.Identifier(table_name))
                     cur.execute(query, (recs, uuid.UUID(i)))
                     con.commit()
             else:
@@ -193,12 +208,58 @@ def insert_cpu(id, cpu_arr):
         con.commit()
 
 
-def insert_gpu(id, gpu_arr):
+def insert_into_table(id, recs_arr, types):
+    if types == 'cpu':
+        table_name = 'recommendation_cpu'
+    else:
+        table_name = 'recommendation_gpu'
+
     psycopg2.extras.register_uuid()
-    for gpu in gpu_arr:
-        query = ("""INSERT INTO recommendation_gpu (product_id, recommended_id) VALUES (%s, %s);""")
-        cur.execute(query,(id, gpu))
+    for rec in recs_arr:
+        query = sql.SQL("INSERT INTO {} (product_id, recommended_id) VALUES (%s, %s);")
+                    .format(sql.Identifier(table_name))
+        cur.execute(query,(id, rec))
         con.commit()
+
+def generate_recs(cur, con, types):
+    wishlist_products = get_wishlist(cur, con, types)
+
+    global products
+    products = get_all_products(cur, con, types)
+
+    vectorizor = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0)
+    tfidf_matrix = vectorizor.fit_transform(products['all_features'])
+
+    global similarity
+    similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+    
+    global rec
+    rec = pd.Series(products['product_id'])
+    
+    temp = []
+    global products
+    products.set_index('product_id', inplace=True)
+    wishlist_products.reset_index()
+    for i, pro in products.iterrows():
+        for j, wish in wishlist_products.iterrows():
+            if i == wish['product_id']:
+                temp.append((wish['product_id'], pro['type']))
+
+    wishlist = pd.DataFrame(temp, columns = ['product_id', 'type'])
+    recs = get_recommendations(wishlist)
+
+    wishlist['recommendations'] = recs
+    wishlist.set_index('product_id', inplace=True)
+
+    new_recs = update_rec_table(wishlist, types)
+
+    if new_recs is not None:
+        df_recs = pd.DataFrame(new_recs, columns=['id', 'recs'])
+        
+        for i, row in df_recs.iterrows():
+            insert_into_table(row['id'], row['recs'], types)
+
+    con.close()
 
 
 #if a change was made to the products table
@@ -208,43 +269,40 @@ def generate_recommendations(curr, conn):
 
     global cur
     cur = curr
-    wishlist_products = get_wishlist(cur, con)
-
-    global all_products
-    all_products = get_all_products(cur, con)
-
-
-    #all_products.set_index('id', inplace=True)
-    all_products.rename(columns={'id': 'product_id'}, inplace=True)
-    all_products['all_features'] = all_products['brand'] +" "+ all_products['retailer'] +" "+ all_products['description_cl']
-    all_products['all_features'] = all_products['all_features'].apply(lambda x: x.lower())
     
-    vectorizor = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0)
-    tfidf_matrix = vectorizor.fit_transform(all_products['all_features'])
+    generate_recs(cur, con, 'cpu')
+    generate_recs(cur, con, 'gpu')
+    # #all_products.set_index('id', inplace=True)
+    # all_products.rename(columns={'id': 'product_id'}, inplace=True)
+    # all_products['all_features'] = all_products['brand'] +" "+ all_products['retailer'] +" "+ all_products['description_cl']
+    # all_products['all_features'] = all_products['all_features'].apply(lambda x: x.lower())
+    
+    # vectorizor = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0)
+    # tfidf_matrix = vectorizor.fit_transform(all_products['all_features'])
 
-    global similarity
-    similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+    # global similarity
+    # similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
     
-    global rec
-    rec = pd.Series(all_products['product_id'])
+    # global rec
+    # rec = pd.Series(all_products['product_id'])
     
-    gpu_recs, cpu_recs = get_all_product_recommendations(wishlist_products)
+    # gpu_recs, cpu_recs = get_all_product_recommendations(wishlist_products)
     
     
-    new_gpu = update_gpu(gpu_recs)
-    new_cpu = update_cpu(cpu_recs)
+    # new_gpu = update_gpu(gpu_recs)
+    # new_cpu = update_cpu(cpu_recs)
 
-    if new_gpu is not None:
-        df_gpu = pd.DataFrame(new_gpu, columns=['id', 'recs'])
+    # if new_gpu is not None:
+    #     df_gpu = pd.DataFrame(new_gpu, columns=['id', 'recs'])
         
-        for i, row in df_gpu.iterrows():
-            insert_gpu(row['id'], row['recs'])
+    #     for i, row in df_gpu.iterrows():
+    #         insert_gpu(row['id'], row['recs'])
 
-    if new_cpu is not None:
-        df_cpu = pd.DataFrame(new_cpu, columns=['id', 'recs'])
-        for i, row in df_cpu.iterrows():
-            insert_cpu(row['id'], row['recs'])
-    con.close()
+    # if new_cpu is not None:
+    #     df_cpu = pd.DataFrame(new_cpu, columns=['id', 'recs'])
+    #     for i, row in df_cpu.iterrows():
+    #         insert_cpu(row['id'], row['recs'])
+    # con.close()
 
 
 #if a change was made to a users wishlist
